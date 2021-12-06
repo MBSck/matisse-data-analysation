12d94f36fe68f29d35ef3c0f6427c331bb4bbe8f
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2021-12-06 14:20:14 +0100

    Update constant.py
    Rename and reformat the constants

--- modelling/constant.py
+++ modelling/constant.py
@@ -2,9 +2,9 @@
 
 __author__ = "Marten Scheuck"
 
-PLANCK: float = 6.62607015e-34          # Planck's constant in SI [J/Hz]
-SPEED_OF_LIGHT: float = 299792458       # Speed of light in SI [m/s]
-BOLTZMAN: float = 1.380649e-23          # Boltzman's constant in SI [J/K] 
+PLANCK_CONST: float = 6.62607015e-34        # Planck's constant in SI [J/Hz]
+SPEED_OF_LIGHT: float = 2.99792458e8        # Speed of light in SI [m/s]
+BOLTZMAN_CONST: float = 1.380649e-23        # Boltzman's constant in SI [J/K] 
 
 if __name__ == "__main__":
     print(BOLTZMAN)
