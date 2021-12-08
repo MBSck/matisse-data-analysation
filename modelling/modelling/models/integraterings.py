Fix InclinedDisk Numerical
The numerical integration works now

modelling/modelling/models/integraterings.py
@@ -1,102 +1,118 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from modelling.functionality.utilities import Model, timeit, set_size, set_uvcoords, \
         temperature_gradient, blackbody_spec
+
+from modelling.models.ring import Ring
 
 class IntegrateRings:
     """Adds 2D rings up/integrates them to create new models (e.g., a uniform disk or a ring structure)
 
     ...
 
+    Parameters
+    ----------
+    min_radius: int
+    max_radius: int
+    step_size: int
+    q: float
+    T_0: int
+    wavelength: float
+
+    Returns
+    -------
+    None
+
     Methods
     -------
     add_rings2D():
         This adds the rings up to various models and shapes
-    uniform_disk():
+    uniformly_bright_disk():
         Calls the add_rings() function with the right parameters to create a uniform disk
-    disk():
+    optically_thin_disk():
         Calls the add_rings() function with the right parameters to create a disk with an inner rim
+    optically_thick_disk():
+        ...
+    rimmed_disk():
+        ...
     """
-    def __init__(self, size_model: int) -> None:
-        self.size = size_model
-
-    def add_rings(self, min_radius: int, max_radius: int, step_size: int, q: float, T_0: int, wavelength: float, do_flux: bool = True, optical_depth: float = 1., optically_thick: bool = False) -> None:
-        """This adds the rings up to various models
-
-        Parameters
-        ----------
-        min_radius: int
-        max_radius: int
-        step_size: int
-        q: float
-        T_0: int
-        wavelength: float
-
-        Returns
-        -------
-        None
-        """
+    def add_rings(self, size, min_radius: int, max_radius: int, step_size: int,
+                  q: float, T_0: int, wavelength: float, do_flux: bool = True,
+                  optical_depth: float = 1., inc_angle: int = 0,
+                  pos_angle_axis: int = 0, pos_angle_ellipsis: int = 0) -> None:
+        """This adds the rings up to various models"""
         # TODO: Make this more performant -> Super slow
-        output_lst = np.zeros((self.size, self.size))
 
+        output_lst = np.zeros((size, size))
         # Check if flux should be added to disk
         if do_flux:
             for i in range(min_radius+1, max_radius+2, step_size):
                 # Optically thick (t_v >> 1 -> e^(-t_v)=0, else optically thin)
-                if optically_thick:
-                    flux = blackbody_spec(i, q, min_radius, T_0, wavelength)
-                else:
-                    flux = blackbody_spec(i, q, min_radius, T_0, wavelength)*(1-np.exp(-optical_depth))
-
-                ring_array = Ring().eval_model(self.size, i)
+                flux = blackbody_spec(i, q, min_radius, T_0, wavelength)*(1-np.exp(-optical_depth))
+                ring_array = Ring().eval_numerical(size, i, inc_angle=inc_angle, pos_angle_axis=pos_angle_axis, pos_angle_ellipsis=pos_angle_ellipsis)
                 output_lst[np.where(ring_array > 0)] = flux/(np.pi*max_radius)
         else:
             for i in range(min_radius+1, max_radius+2, step_size):
-                ring_array = Ring().eval_model(self.size, i)
+                ring_array = Ring().eval_numerical(size, i, inc_angle=inc_angle, pos_angle_axis=pos_angle_axis, pos_angle_ellipsis=pos_angle_ellipsis)
                 output_lst[np.where(ring_array > 0)] = 1/(np.pi*max_radius)
 
         return output_lst
 
     @timeit
-    def uniformly_bright_disk(self, radius: int, wavelength: float = 8e-06, q: float = 0.55, T_0: float = 6000, step_size: int = 1) -> np.array:
+    def uniformly_bright_disk(self, size: int, radius: int, wavelength: float = 8e-06, q: float = 0.55, T_0: float = 6000, step_size: int = 1) -> np.array:
         """Calls the add_rings2D() function with the right parameters to create a uniform disk
 
         See also
         --------
         add_rings()
         """
-        return self.add_rings(0, radius, step_size, q, T_0, wavelength, do_flux=False, optical_depth=1., optically_thick=False)
+        return self.add_rings(size, 0, radius, step_size, q, T_0, wavelength, do_flux=False)
 
 
     @timeit
-    def optically_thin_disk(self, radius: int, wavelength: float = 8e-06, q: float = 0.55, T_0: float = 6000, step_size: int = 1) -> np.array:
+    def optically_thin_disk(self, size: int, radius: int, wavelength: float = 8e-06, q: float = 0.55, T_0: float = 6000, step_size: int = 1) -> np.array:
         """Calls the add_rings2D() function with the right parameters to create a uniform disk
 
         See also
         --------
         add_rings()
         """
-        return self.add_rings(0, radius, step_size, q, T_0, wavelength, do_flux=True, optical_depth=0.1, optically_thick=False)
+        return self.add_rings(size, 0, radius, step_size, q, T_0, wavelength, optical_depth=0.1)
 
     @timeit
-    def optically_thick_disk(self, radius: int, wavelength: float = 8e-06, q: float = 0.55, T_0: float = 6000, step_size: int = 1) -> np.array:
+    def optically_thick_disk(self, size: int, radius: int, wavelength: float = 8e-06, q: float = 0.55, T_0: float = 6000, step_size: int = 1) -> np.array:
         """Calls the add_rings2D() function with the right parameters to create a uniform disk
 
         See also
         --------
         add_rings()
         """
-        return self.add_rings(0, radius, step_size, q, T_0, wavelength, do_flux=True,  optical_depth=100., optically_thick=True)
+        return self.add_rings(size, 0, radius, step_size, q, T_0, wavelength, optical_depth=100.)
 
 
     @timeit
-    def rimmed_disk(self, inner_radius: int, outer_radius: int, wavelength: float = 8e-06, q: float = 0.55, T_0: float = 6000, step_size: int = 1, do_flux: bool = True, optical_depth: float = 0.01, optically_thick: bool = False) -> np.array:
+    def rimmed_disk(self, size: int, inner_radius: int, outer_radius: int, wavelength: float = 8e-06, q: float = 0.55, T_0: float = 6000, step_size: int = 1, do_flux: bool = True, optical_depth: float = 0.01) -> np.array:
         """Calls the add_rings2D() function with the right parameters to create a disk with a inner ring
 
         See also
         --------
         add_rings()
         """
-        return self.add_rings(inner_radius, outer_radius, step_size, q, T_0, wavelength, do_flux, optical_depth, optically_thick)
+        return self.add_rings(size, inner_radius, outer_radius, step_size, q, T_0, wavelength, do_flux, optical_depth)
 
+    @timeit
+    def inclined_disk(self, size: int, inner_radius: int, outer_radius: int, wavelength: float = 8e-06, q: float = 0.55, T_0: float = 6000, step_size: int = 1, do_flux: bool = True, optical_depth: float = 0.01) -> np.array:
+        """Calls the add_rings2D() function with the right parameters to create a disk with a inner ring
+
+        See also
+        --------
+        add_rings()
+        """
+        return self.add_rings(size, inner_radius, outer_radius, step_size, q, T_0, wavelength, inc_angle=90, pos_angle_axis=45, pos_angle_ellipsis=45)
+
+if __name__ == "__main__":
+    integ = IntegrateRings()
+    plt.imshow(integ.inclined_disk(512, 20, 50))
+    plt.show()
 

modelling/modelling/models/ring.py
@@ -1,6 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-from modelling import Model, timeit, set_size, set_uvcoords, delta_fct
+from scipy.special import j0
+
+from modelling.functionality.utilities import Model, timeit, set_size, set_uvcoords, delta_fct
 
 class Ring(Model):
     """Infinitesimal thin ring model
@@ -15,8 +18,8 @@ class Ring(Model):
         The major determines the radius/cutoff of the model
     step: float
         The stepsize for the np.array that constitutes the x, y-axis
-    flux: float
-        The flux of the system
+    inc_angle: int
+        The angle of the ring's i)nclination, defaults to 0.
     centre
         The centre of the model, will be automatically set if not determined
 
@@ -28,7 +31,7 @@ class Ring(Model):
         Evaluates the visibilities of the model
     """
     @timeit
-    def eval_model(self, size: int, major: int, step: int = 1, flux: float = 1., centre: bool = None) -> np.array:
+    def eval_model(self, size: int, major: int, inc_angle: int = 0, step: int = 1, centre: bool = None) -> np.array:
         """Evaluates the model. In case of zero divison error, the major will be replaced by 1
 
         Returns
@@ -39,9 +42,9 @@ class Ring(Model):
         radius = set_size(size, step, centre)
 
         try:
-            return np.array([[(flux*delta_fct(j, major/2))/(np.pi*major) for j in i] for i in radius])
+            return np.array([[delta_fct(j, major/2)/(np.pi*major) for j in i] for i in radius])
         except ZeroDivisionError:
-            return np.array([[(flux*delta_fct(j, major/2))/(np.pi) for j in i] for i in radius])
+            return np.array([[delta_fct(j, major/2)/(np.pi) for j in i] for i in radius])
 
     @timeit
     def eval_vis(self, major: int) -> np.array:
@@ -55,4 +58,42 @@ class Ring(Model):
         B = set_uvcoords()
 
         return j0(2*np.pi*major*B)
+
+    @timeit
+    def eval_numerical(self, size: int, outer_radius: int, inner_radius: int = None, inc_angle: int = 0, pos_angle_axis: int = 0, pos_angle_ellipsis: int = 0, centre: bool = None) -> np.array:
+        """Numerically evaluates the ring model"""
+        x = np.arange(0, size)
+        y = x[:, np.newaxis]
+        inc_angle = np.radians(inc_angle)
+        pos_angle_axis = np.radians(pos_angle_axis)
+        pos_angle_measure = np.radians(pos_angle_ellipsis)
+
+        if centre is None:
+            x0 = y0 = size//2
+        else:
+            x0, y0 = centre
+
+        # Calculates the radius from the centre and adds rotation to it
+        xc, yc = x-x0, y-y0
+        a, b = xc*np.sin(pos_angle_ellipsis), yc*np.cos(pos_angle_ellipsis)
+        ar, br = a*np.sin(pos_angle_axis)+b*np.cos(pos_angle_axis), \
+                a*np.cos(pos_angle_axis)-b*np.sin(pos_angle_axis)
+
+        radius = np.sqrt(ar**2+br**2*np.cos(inc_angle))
+
+        # Gets the boundaries of the resulting ellipsis
+        radius[radius > outer_radius] = 0.
+        if inner_radius is None:
+            radius[radius < outer_radius-1] = 0.
+        else:
+            radius[radius < inner_radius] = 0.
+
+        return radius
+
+if __name__ == "__main__":
+    ri = Ring()
+    for i in range(10, 90, 5):
+        inclined_ring =  ri.eval_numerical(512, 50, inc_angle=i, pos_angle_axis=45, pos_angle_ellipsis=45)
+        plt.imshow(inclined_ring)
+        plt.show()
 