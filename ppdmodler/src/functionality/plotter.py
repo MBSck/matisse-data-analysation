42c95adfd6ded573e95b9c7ea222fe2a3dfa2715
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-01-19 11:59:21 +0100

    Update plotter.py

--- ppdmodler/src/functionality/plotter.py
+++ ppdmodler/src/functionality/plotter.py
@@ -168,7 +168,7 @@ class Plotter:
 
         for j in range(4):
             axis = axarr[1, j%4]
-            axis.errorbar(wl*1e6, all_obs[j][0], yerr=np.std(all_obs[j], 0),
+            axis.errorbar(self.wl*1e6, all_obs[j][0], yerr=np.std(all_obs[j], 0),
                           marker='s', capsize=0., alpha=0.9, color='k',
                           label=tel_t3phi[j])
             axis.legend([tel_t3phi[j]], loc=2)
