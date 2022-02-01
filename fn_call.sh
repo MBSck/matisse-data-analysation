5b26eb901bd25b50660081d52f58d8e199f64fa4
Marten Scheuck <martenscheuck@gmail.com>
2022-02-01 16:28:23 +0100

    Removed file

--- fn_call.sh
+++ fn_call.sh
@@ -1,19 +1,28 @@
 #!/bin/env bash
 # Brackets around the code makes it possible to change during running
 # as whole file is parsed before run
+
+# TODO: Make this work for different folders and subfolders, iteration and check results
+
 {
 #change this to point to your oca pipeline
 cd ./oca_pipeline/tools/
 
 #----------define the folders for saving data-----------------
-#location of the data folders (raw, outputs, etc)
-TARGET=openTime/suAur
+# Location of the data folders (raw, outputs, etc)
 DATADIR=/data/beegfs/astro-storage/groups/matisse/scheuck/data
-RAWDIR=$DATADIR/$TARGET/RAW
-CALIBDIR=$RAWDIR
-RESDIR=$DATADIR/$TARGET/PRODUCTS
 
+# Leave empty if only one folder is to be reduced
 TARGETLIST=
+
+# And give specific folder if only folder is to be reduced
+TARGET=openTime/uxOri
+TARGETDIR=$DATADIR/$TARGET
+
+# Will define the folders if TARGETLIST is left empty
+RAWDIR=$TARGETDIR/RAW
+CALIBDIR=$RAWDIR
+RESDIR=$TARGETDIR/PRODUCTS
 
 # Checks if reductions for both bands should be done -> implement that
 DO_BOTH=true
@@ -39,26 +48,26 @@ start=`date +%s`
 
 #----------Do the L-band-------------------------------
 if [ "$CHECK_LBAND" == true ]; then
-	python automaticPipeline.py --dirRaw=$1 --dirCalib=$2 --dirResult=$RESDIR --nbCore=10 --overwrite=TRUE --maxIter=1 --paramL=/corrFlux=FALSE/coherentAlgo=2/compensate=[pb,rb,nl,if,bp,od]/cumulBlock=FALSE/spectralBinning=11/  --skipN  #--tplSTART=2021-02-28T08:13:00
+	python automaticPipeline.py --dirRaw=$1 --dirCalib=$2 --dirResult=$3 --nbCore=10 --overwrite=TRUE --maxIter=1 --paramL=/corrFlux=FALSE/coherentAlgo=2/compensate=[pb,rb,nl,if,bp,od]/cumulBlock=FALSE/spectralBinning=11/  --skipN  #--tplSTART=2021-02-28T08:13:00
 
 	#------ move the results to a labelled folder ----------
-    if [ -d "$RESDIR/lband" ]; then
-	    mv -f $RESDIR/Iter1/*.rb $RESDIR/lband
+    if [ -d "$3/lband" ]; then
+	    mv -f $3/Iter1/*.rb $3/lband
     else
-        mkdir $RESDIR/lband/
-	    mv -f $RESDIR/Iter1/*.rb $RESDIR/lband
+        mkdir $3/lband/
+	    mv -f $3/Iter1/*.rb $3/lband
     fi
 
 #----------Do the N-band-------------------------------
 else
-	python automaticPipeline.py --dirRaw=$1 --dirCalib=$2 --dirResult=$RESDIR --nbCore=4 --overwrite=TRUE --maxIter=1 --paramN=/useOpdMod=TRUE/coherentAlgo=2/corrFlux=FALSE/compensate=[pb,rb,nl,if,bp,od]/coherentIntegTime=0.2/cumulBlock=FALSE/spectralBinning=11/replaceTel=2  --skipL 
+	python automaticPipeline.py --dirRaw=$1 --dirCalib=$2 --dirResult=$3 --nbCore=4 --overwrite=TRUE --maxIter=1 --paramN=/useOpdMod=TRUE/coherentAlgo=2/corrFlux=FALSE/compensate=[pb,rb,nl,if,bp,od]/coherentIntegTime=0.2/cumulBlock=FALSE/spectralBinning=11/replaceTel=2  --skipL 
 
 	#------ move the results to a labelled folder ----------
-    if [ -d "$RESDIR/nband" ]; then
-	    mv -f $RESDIR/Iter1/*.rb $RESDIR/nband
+    if [ -d "$3/nband" ]; then
+	    mv -f $3/Iter1/*.rb $3/nband
     else
-        mkdir $RESDIR/nband/
-	    mv -f $RESDIR/Iter1/*.rb $RESDIR/nband
+        mkdir $3/nband/
+	    mv -f $3/Iter1/*.rb $3/nband
     fi
 fi
 
@@ -67,19 +76,23 @@ runtime=$((end-start))
 printf "Finished calibration in %f sec" $runtime
 }
 
-# Make directories if they are not yet exisiting
-make_directory $TARGETDIR $CALIBDIR
-
 if [ -z $TARGETLIST];then
-    echo works
-    do_reduction $RAWDIR $CALIBDIR
+    # Make directories if they are not yet exisiting
+    make_directory $TARGETDIR $CALIBDIR $RESDIR
+
+    do_reduction $RAWDIR $CALIBDIR $RESDIR
 else
     for i in $TARGETLIST
     do
-        TEMPRAW=$RAWDIR/$i
-        TEMPCALIB=$CALIBDIR/$i
+        # Temp variables that set the folders
+        TEMPRAW=$TARGETDIR/$i/RAW
+        TEMPCALIB=$TEMPCALIB
+        TEMPRES=$TARGETDIR/$i/PRODUCTS
 
-        do_reduction $TEMPRAW $TEMPCALIB
+        # Make directories if they are not yet exisiting
+        make_directory $TEMPRAW $TEMPCALIB
+
+        do_reduction $TEMPRAW $TEMPCALIB $TEMPRES
     done
 fi
 
