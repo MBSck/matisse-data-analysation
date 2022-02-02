34e349811888faa28daa4a38b9b0c9686c188d3a
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-02-02 16:51:48 +0100

    Add to mcmc.py
    Add functionality that makes it run faster and compares smaller models to each other

--- ppdmodler/src/functionality/mcmc.py
+++ ppdmodler/src/functionality/mcmc.py
@@ -26,40 +26,36 @@ from typing import Any, Dict, List, Union, Optional
 
 from src.models import Gauss2D
 from src.functionality.readout import ReadoutFits
-from src.functionality.utilities import get_model2px_scaling
+from src.functionality.utilities import correspond_uv2model
 
-def model(theta: np.ndarray, sampling: int, wavelength: float, B:
-          Optional[np.ndarray] = None, limits: Optional[List[int]] = None):
+def model(theta: np.ndarray, sampling: int, wavelength: float) -> np.ndarray:
     """The model defined for the MCMC process"""
     fwhm = theta
-    if B is not None:
-        model = Gauss2D().eval_vis(fwhm, B=B, wavelength=wavelength, limits=limits)
-    model = Gauss2D().eval_vis(fwhm,sampling=sampling, wavelength=wavelength, limits=limits)
+    model = Gauss2D().eval_vis(fwhm, sampling, wavelength)
     return model
 
-def lnlike(theta: np.ndarray, sampling: int, wavelength: float, B: np.ndarray,
-           Berr: float, uv_ind: np.ndarray, limits: List[int]):
+def lnlike(theta: np.ndarray, sampling: int, wavelength: float, vis2data: np.ndarray,
+           vis2err: np.ndarray, uvcoords: np.ndarray):
     """Takes theta vector and the x, y and the yerr of the theta.
     Returns a number corresponding to how good of a fit the model is to your
     data for a given set of parameters, weighted by the data points. I.e. it is
     more important"""
-    Bm = model(theta, sampling, wavelength, None, limits)
-    B = model(theta, sampling, wavelength, B, None)
-    print(Bm, B)
-    return -0.5*np.sum((B-Bm)**2)
+    vis2datamod = map(lambda x: x*np.conj(x), [model(theta, sampling, wavelength)])
+    vis2datamod = correspond_uv2model(vis2datamod, uvcoords, True)
+    return -0.5*np.sum((vis2data-vis2datamod)**2/vis2err)
 
 def lnprior(theta):
     """This function checks if all variables are within their priors (it is
     setting the same). If all priors are satisfied it needs to return '0.0' and
     if not then '-np.inf'"""
     fwhm = theta
-    if fwhm < 1000 and fwhm > 1:
+    if fwhm < 1000. and fwhm > 0.1:
         return 0.0
     else:
         return -np.inf
 
-def lnprob(theta: np.ndarray, sampling: int, wavelength: float, B: np.array,
-           Berr: float, uv_ind: np.ndarray, limits: List[int]):
+def lnprob(theta: np.ndarray, sampling: int, wavelength: float, vis2data: np.array,
+           vis2err: np.ndarray, uvcoords: np.ndarray) -> np.ndarray:
     """This function runs the lnprior and checks if it returned -np.inf, and
     returns if it does. If not, (all priors are good) it returns the inlike for
     that model (convention is lnprior + lnlike)
@@ -75,22 +71,22 @@ def lnprob(theta: np.ndarray, sampling: int, wavelength: float, B: np.array,
     lp = lnprior(theta)
     if not lp == 0.0:
         return -np.inf
-    return lp + lnlike(theta, sampling, wavelength, B, Berr, uv_ind, limits)
+    return lp + lnlike(theta, sampling, wavelength, vis2data, vis2err)
 
-def main(p0, nwalkers, niter_burn, niter, ndim, lnprob, data) -> np.array:
+def main(p0, nwalkers_burn, nwalkers, niter_burn, niter, ndim, lnprob, data) -> np.array:
     """"""
     # The EnsambleSampler gets the parameters. The args are the args put into the
     # lob_prob function. Additional parameter a can be used for the stepsize. None is
     # the default 
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=data)
+    sampler = emcee.EnsembleSampler(nwalkers_burn, ndim, lnprob, args=data)
 
     # Burn-in of the sampler. Explores the parameter space. The walkers get settled
     # into the maximum of the density. Saves the walkers in the state variable
     print("Running burn-in...")
     p0 = sampler.run_mcmc(p0, niter_burn)
 
-    # Resets the chain to remove burn in samples
-    sampler.reset()
+    # Resets the chain to remove burn in samples and sets the walkers lower
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=data)
 
     # Do production. Starts from the final position of burn-in chain (rstate0 is
     # the state of the internal random number generator)
@@ -99,6 +95,7 @@ def main(p0, nwalkers, niter_burn, niter, ndim, lnprob, data) -> np.array:
 
     return sampler, pos, prob, state
 
+def test_model(sampler) -> None:
     # Another test of whether or not the sampling went well is the mean acceptance
     # fraction and the integrated autocorrelation time
     # Acceptance fraction has an entry for each walker -> It is a vector with the
@@ -137,37 +134,33 @@ if __name__ == "__main__":
     fwhm = 10.
     initial = np.array([fwhm])
 
-    # TODO: Make model give the frequency scaling in np.diff(sampling), roll
-    # etc.
-
-    # Readout Fits
+    # Readout Fits for real data
     f = "/Users/scheuck/Documents/PhD/matisse_stuff/ppdmodler/assets/TARGET_CAL_INT_0001bcd_calibratedTEST.fits"
     readout = ReadoutFits(f)
+    vis2data, vis2err, vis2sta = readout.get_vis2()
     uvcoords = readout.get_uvcoords()
+
+    # Correspond uv-coords to indicies of the model with certain size/sampling
+    # rate
 
     # Sets the wavelength the model is to be evaluated at
     wavelength = 8e-06
 
-    # Realdata
-    B = [np.sqrt((u/wavelength)**2+(v/wavelength)**2) for u, v in uvcoords]
-    # vis = 
-
     # Set the data to be fitted. Error arbitrary, set to 1%
-    Berr = 0.01*np.mean(B)
-    data = (sampling, wavelength, B, Berr, uv_ind, limits)
+    data = (sampling, wavelength, vis2data, vis2err, uvcoords)
 
     # The number of walkers (must be even) and the number of dimensions/parameters
-    nwalkers, ndim = 250, len(initial)
+    nwalkers_burn, nwalkers, ndim = 250, 50, len(initial)
 
     # Sets the steps of the burn-in and the max. steps
-    niter_burn, niter = 1, 5
+    niter_burn, niter = 100, 10000
 
     # This vector defines the starting points of each walker for the amount of
     # dimensions
     p0 = np.random.rand(nwalkers, ndim)
 
     # This calls the MCMC fitting
-    sampler, pos, prob, state = main(p0, nwalkers, niter_burn, niter, ndim, lnprob, data)
+    sampler, pos, prob, state = main(p0, nwalkers_burn, nwalkers, niter_burn, niter, ndim, lnprob, data)
 
     # This plots the resulting model
     model_plot(sampler, sampling, 8e-06)
--- ppdmodler/src/functionality/utilities.py
+++ ppdmodler/src/functionality/utilities.py
@@ -51,35 +51,13 @@ def delta_fct(x: Union[int, float], y: Union[int, float]) -> int:
     """
     return 1 if x == y else 0
 
-def compare_arrays(arr1: np.array, arr2: np.array,
-                   rtol: Union[int, float] = 1e-05, atol: Union[int, float] = 1e-08) -> [np.array, np.array]:
-    """Compares two arrays for their differences in value and also if they are equal in a certain tolerance
-
-    Parameters
-    ----------
-    array1: np.array
-    array2: np.array
-    rtol: int | float
-        relative tolerance of difference
-    atol: int | float
-        absolute tolerance of difference
-
-    Returns
-    -------
-    np.array
-        The numerical difference between the arrays
-    np.array
-        An array of booleans that contain True/False if their difference is within a certain tolerance
-    """
-    return array1-array2, np.isclose(array1, array2, rtol=rtol, atol=atol)
-
-def mas2rad(angle: Optional[Union[int, float]] = None):
+def mas2rad(angle: Optional[Union[int, float, np.ndarray]] = None):
     """Returns a given angle in mas/rad or the pertaining scaling factor
 
     Parameters
     ----------
-    angle: [int | float] | None
-        The input angle
+    angle: int | float | np.ndarray, optional
+        The input angle(s)
 
     Returns
     -------
@@ -90,97 +68,65 @@ def mas2rad(angle: Optional[Union[int, float]] = None):
         return np.deg2rad(1/3.6e6)
     return np.deg2rad(angle/3.6e6)
 
-def get_model2px_scaling(sampling: float, wavelength: float):
+def get_px_scaling(axis: np.ndarray):
     """Gets the model's scaling from its sampling rate
 
     Parameters
     ----------
-    sampling: float
-        The sampling of the uvcoords
+    axis: np.ndarray
+        The axis from which the scaling is to be computed. For FFT it is
+        np.fft.fftfreq and for analytical model set_uvcoords
 
     Return
     ------
     float
         The px to uv-coord scaling
     """
-    B = set_uvcoords(sampling, wavelength)
-    roll = np.floor(len(B)/2).astype(int)
-    freq = np.roll(B, roll, axis=0)
-    return np.diff(freq)[0][0]
+    roll = np.floor(len(axis)/2).astype(int)
+    axis = np.roll(axis, roll, axis=0)
+    return np.diff(axis)[0][0]
 
-def scale_uv2px(sampling: int, u: np.array,
-                v: np.array, wavelength: float) -> np.array:
-    """Takes uv-coords and the image/model's scaling to rescale them into
-    radians per px
-
-    Parameters
-    ----------
-    sampling: int
-        The sampling of the image
-    u: np.array
-        The u-coords
-    v: np.array
-        The v-coords
-    wavelength: float
-        The wavelength at which the uv-coords are sampled
-
-    Returns
-    -------
-    tuple
-        The rescaled uv-coords as a tuple of np.arrays
-    """
-    u, v = map(lambda x: get_model2px_scaling(sampling)*(mas2rad(x)/wavelength), (u, v))
-    return u, v
-
-def get_scaling_px2metr(scaling: float, wavelength: float) -> float:
-    """Calculates the frequency scaling from the axis of an input image/model and returns it in meters baseline per pixel
+def get_scaling_px2metr(model: np.ndarray, wavelength: float, uvcoords:
+                        np.ndarray) -> float:
+    """Calculates the axis scaling from the axis of an input image/model and
+    returns it in meters baseline per pixel. Returns the uv-coords
 
     Parameters
     ----------
-    scaling: float
-        The scaling of the image/modelx-axis to px
+    axis: np.ndarray
+        The axis from which the scaling is to be computed. For FFT it is
+        np.fft.fftfreq and for analytical model set_uvcoords
     wavelength: float
         The wavelength at which the scaling should take place
-    """
-    return (array_scaling/mas2rad())*wavelength
-
-def rescale_uvcoords(self, model_size: int, scaling: float, uvcoords: np.array) -> np.array:
-    """Rescaled the uv-coords with the scaling factor and the max image size as
-    well as to radians. Applicable for FFT, but not for analytical model as it
-    is already in radians
-
-    Parameters
-    ----------
-    model_size: int
-        The dimensions of the model
-    scaling: float
-        The scaling of the image/model x-axis to px
     uvcoords: np.array
-        The uvcoords to be rescaled
+        The real uvcoords
 
     Returns
     -------
-    np.array
-        The rescaled uvcoords
+    uvcoords: np.ndarray
+        The rescaled uv-coords
     """
-    return uvcoords/(get_scaling_px2metr(scaling, wavelength)*model_size)
+    axis_size = len(axis)
+    scaling = (get_px_scaling(axis)/mas2rad())*wavelength*axis_size
+    return uvcoords/scaling
 
-def get_distance(self, axis: np.array, uvcoords: np.array) -> np.array:
+def get_distance(self, axis: np.ndarray, uvcoords: np.ndarray) -> np.ndarray:
     """Calculates the norm for a point. Takes the freq and checks both the
-    u- and v-coords against it (works only for models/image that have the same length in both dimensions).
+    u- and v-coords against it.
+    Works only for models/image that have the same length in both dimensions.
 
     The indices of the output list evaluate to the indices of the input list
 
     Parameters
     ----------
-    axis: np.array
+    axis: np.ndarray
         The axis of which the distance is to be calculated
-    uvcoords: np.array
+    uvcoords: np.ndarray
         The uvcoords that should be cross referenced
 
     Returns
     -------
-    np.array
+    np.ndarray
         The indices of the closest matching points
     """
     # This makes a list of all the distances in the shape (size_img_array, uv_coords)
@@ -190,12 +136,16 @@ def get_distance(self, axis: np.array, uvcoords: np.array) -> np.array:
     indices_lst = [[j for j, o in enumerate(i) if o == np.min(np.array(i))] for i in distance_lst]
     return np.ndarray.flatten(np.array(indices_lst))
 
-def correspond_uv2model(uvcoords: np.array, dis: bool = False, intpol: bool = False) -> List:
+def interpolate():
+    ...
+
+def correspond_uv2model(model: np.ndarray, uvcoords: np.array, dis: bool = False, intpol: bool = False) -> List:
     """This gets the indicies of rescaled given uvcoords to a image/model with
     either euclidean distance or interpolation and returns their vis2 values
 
     Parameters
     ----------
+
     uvcoords: np.array
         The to the image's size rescaled uvcoords
     dis: bool
@@ -215,11 +165,7 @@ def correspond_uv2model(uvcoords: np.array, dis: bool = False, intpol: bool = Fa
     if intpol:
         ...
 
-    return [ft[i[0]][i[1]] for i in uv_ind]
-
-def readout_model_px2uvcoords(self):
-    """This function reads all the pixels into uv coords with the scaling factor"""
-    ...
+    return [model[i[0]][i[1]] for i in uv_ind]
 
 def set_size(size: int, sampling: Optional[int] = None,  centre: Optional[int] = None, pos_angle: Optional[int] = None) -> np.array:
     """
@@ -230,18 +176,18 @@ def set_size(size: int, sampling: Optional[int] = None,  centre: Optional[int] =
     size: int
         Sets the size of the model image and implicitly the x-, y-axis.
         Size change for simple models functions like zero-padding
-    sampling: int
+    sampling: int, optional
         The sampling of the object-plane
-    centre: bool
+    centre: bool, optional
         A set centre of the object. Will be set automatically if default 'None' is kept
-    pos_angle: int | None
+    pos_angle: int | None, optional
         This sets the positional angle of the ellipsis if not None
 
     Returns
     -------
     radius: np.array
     """
-    if (sampling is None) or (sampling < size):
+    if (sampling is None):
         sampling = size
 
     x = np.linspace(0, size, sampling)
@@ -260,7 +206,6 @@ def set_size(size: int, sampling: Optional[int] = None,  centre: Optional[int] =
 
     return np.sqrt(xc**2+yc**2)*mas2rad()
 
-@timeit
 def set_uvcoords(sampling: int, wavelength: float,) -> np.array:
     """Sets the uv coords for visibility modelling
 
@@ -275,12 +220,9 @@ def set_uvcoords(sampling: int, wavelength: float,) -> np.array:
     -------
     baseline: ArrayLike
     """
-    if sampling < 300:
-        sampling = 300
-
     B = np.linspace(-150, 150, sampling)
 
-    # Star overhead sin(theta_0)=1
+    # Star overhead sin(theta_0)=1 position
     u, v = B/wavelength, B[:, np.newaxis]/wavelength
 
     return np.sqrt(u**2+v**2)
@@ -336,58 +278,8 @@ def blackbody_spec(radius: float, q: float, r_0: Union[int, float], T_0: int, wa
     return numerator/divisor
 
 
-
-def do_plot(input_models: List[np.array], *args, ffft: bool = False, ft: Optional[np.array],
-            mod: bool = False, vis: bool = False, both: bool = False) -> None:
-    """Simple plot function for the models
-
-    Parameters
-    ----------
-    input_model: np.arry
-        Model input, in form of a 2D-np.array
-    args
-        Different model inputs
-    fft: bool
-        By default False, if toggled plots FFT instead of models
-    ft: np.array | None
-        The FFT to be plotted if fft is True
-    mod: bool
-        By default False, if toggled plots model
-    vis: bool
-        By default False, if toggled plots visibilities (FFT)
-    both: bool
-        By default False, if toggled plots both model and vis
-
-    Returns
-    -------
-    None
-    """
-    # TODO: Make this take any number of arguments as well as any number of models
-    for model in input_models:
-        model = model()
-        if fft:
-            ...
-        else:
-            if mod:
-                plt.imshow(model.eval_model(*args))
-            if vis:
-                plt.imshow(model.eval_vis(*args), extent=(-150, 150, -150, 150))
-            if both:
-                fig, (ax1, ax2) = plt.subplots(1, 2)
-                ax1.imshow(model.eval_model(*args))
-                ax2.imshow(model.eval_vis(*args), extent=(-150, 150, -150, 150))
-
-        ax1.set_title("Model")
-        ax1.set_xlabel("[px]")
-
-        ax2.set_title("Vis")
-        ax2.set_xlabel("u[m]")
-        ax2.set_ylabel("v[m]")
-        plt.show()
-
-
 if __name__ == "__main__":
-    ...
+    print(set_uvcoords(10, 8e-06))
     # readout = ReadoutFits("TARGET_CAL_INT_0001bcd_calibratedTEST.fits")
 
     # print(readout.get_uvcoords_vis2, "uvcoords")
