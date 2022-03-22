6f7017b68acdf7287475e8f3aa566d56baa3d27b
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-03-22 20:22:42 +0100

    Add functionality to get both bands reduced

--- dataReduction/do_complete_reduction.py
+++ dataReduction/do_complete_reduction.py
@@ -27,48 +27,54 @@ def set_script_arguments(do_flux: bool, array: str,
 
     return (paramL_lst, paramN_lst)
 
-def reduction_pipeline(rawdir: str, calibdir: str, resdir: str, do_l: bool,
+def reduction_pipeline(rawdir: str, calibdir: str, resdir: str,
                        array: str) -> int:
     """Runs the pipeline for coherent and incoherent reduction."""
     if not os.path.exists(resdir):
         os.makedirs(resdir)
 
+    overall_start_time = time.time()
+
     for i in [True, False]:
-        # Takes the time at the start of execution
-        tic = time.perf_counter()
+        for j in [True, False]:
+            # Takes the time at the start of execution
+            start_time = time.time()
 
-        path_lst = ["coherent" if i else "incoherent", "lband" if do_l else "nband" ]
-        path = "/".join(path_lst)
-        paramL, paramN = set_script_arguments(i, array)
-        skipL, skipN = int(not do_l), int(do_l)
+            path_lst = ["coherent" if j else "incoherent", "lband" if i else "nband" ]
+            path = "/".join(path_lst)
+            paramL, paramN = set_script_arguments(j, array)
+            skipL, skipN = int(not i), int(i)
 
-        subdir = os.path.join(resdir, path)
-        if not os.path.exists(subdir):
-            os.makedirs(subdir)
+            subdir = os.path.join(resdir, path)
+            if not os.path.exists(subdir):
+                os.makedirs(subdir)
 
-        mp.mat_autoPipeline(dirRaw=rawdir, dirResult=resdir,
-                                  dirCalib=calibdir,
-                                  nbCore=6, resol='',
-                                  paramL=paramL, paramN=paramN,
-                                  overwrite=0, maxIter=1,
-                                  skipL=skipL, skipN=skipN)
-        try:
-            os.system(f"mv -f {os.path.join(resdir, 'Iter1/*.rb')} {subdir}")
-        except Exception as e:
-            print(e)
+            mp.mat_autoPipeline(dirRaw=rawdir, dirResult=resdir,
+                                      dirCalib=calibdir,
+                                      nbCore=6, resol='',
+                                      paramL=paramL, paramN=paramN,
+                                      overwrite=0, maxIter=1,
+                                      skipL=skipL, skipN=skipN)
+            try:
+                os.system(f"mv -f {os.path.join(resdir, 'Iter1/*.rb')} {subdir}")
+            except Exception as e:
+                print(e)
 
-        # Takes the time at end of execution
-        toc = time.perf_counter()
-        print(f"Executed the {path_lst[0]} {path_lst[1]} reduction in {tic-tic}"
-              " seconds")
+            # Takes the time at end of execution
+            end_time = time.time()
+            print(f"Executed the {path_lst[0]} {path_lst[1]} reduction in"
+                   " {start_time-end_time} seconds")
+
+    overall_end_time = time.time()
+    print(f"Executed the overall reduction in {overall_start_time-overall_end_time}"
+          " seconds")
 
     return 0
 
 if __name__ == "__main__":
     rawdir = "/data/beegfs/astro-storage/groups/matisse/scheuck/data/GTO/hd142666/RAW/20190514"
     calibdir = rawdir
-    resdir = "/data/beegfs/astro-storage/groups/matisse/scheuck/data/GTO/hd142666/PRODUCTS/test"
+    resdir = "/data/beegfs/astro-storage/groups/matisse/scheuck/data/GTO/hd142666/PRODUCTS/20190514"
 
-    reduction_pipeline(rawdir, calibdir, resdir, True, "UT")
-    reduction_pipeline(rawdir, calibdir, resdir, False, "UT")
+    reduction_pipeline(rawdir, calibdir, resdir, "UT")
 
