17079f5013dbddd0bfca5ba5d20dcd3877a7e623
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-03-25 11:43:04 +0100

    Update do_calibration.py

--- dataReduction/do_calibration.py
+++ dataReduction/do_calibration.py
@@ -25,20 +25,29 @@ from fluxcal import fluxcal
 #                            deafult: '' (= no figure made)
 
 if __name__ == "__main__":
-    base_path = "/data/beegfs/astro-storage/groups/matisse/scheuck/data/GTO/hd142666/PRODUCTS/20190514/coherent/"
-    inputfile_sci = "nband/mat_raw_estimates.2019-05-14T05_28_03.AQUARIUS.rb/TARGET_RAW_INT_0001.fits"
-    inputfile_cal = "nband/mat_raw_estimates.2019-05-14T04_52_11.AQUARIUS.rb/CALIB_RAW_INT_0001.fits"
+    # Gets the base paths
+    base_path = "/data/beegfs/astro-storage/groups/matisse/scheuck/data/GTO/hd142666/PRODUCTS/20190514/coherent/nband"
+    inputfile_sci = "mat_raw_estimates.2019-05-14T05_28_03.AQUARIUS.rb/TARGET_RAW_INT_0001.fits"
+    inputfile_cal = "mat_raw_estimates.2019-05-14T04_52_11.AQUARIUS.rb/CALIB_RAW_INT_0001.fits"
+    inputfile_sci = os.path.join(base_path, inputfile_sci)
+    inputfile_cal = os.path.join(base_path, inputfile_cal)
 
-    output_dir = 
-    outputfile = "TARGET_CAL_INT_0001.fits"
+    # Formats the name of the directories for the new cal
+    dir_name, time_sci, band = os.path.dirname(inputfile_sci).split('.')[:-1]
+    dir_name = dir_name.split('/')[-1].replace("raw", "cal")
+    time_cal = os.path.dirname(inputfile_cal).split('.')[-3]
+    output_dir = os.path.join(base_path, "calib", '.'.join([dir_name, time_cal, band, time_sci, "rb"]))
+    output_file = "TARGET_CAL_INT_0001.fits"
+    outputh_path = os.path.join(output_dir, output_file)
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
+    # Datapath of the calib directories
     cal_database_dir = "calib_spec"
     cal_database_files = ['vBoekelDatabase.fits', 'calib_spec_db_v10.fits',
                           'calib_spec_db_v10_supplement.fits']
     cal_database_paths = [os.path.join(cal_database_dir, i) for i in cal_database_files]
 
-    fluxcal(inputfile_sci, inputfile_cal, outputfile,\
+    fluxcal(inputfile_sci, inputfile_cal, outputh_path,\
             cal_database_paths, mode='corrflux',output_fig_dir=output_dir)
-
