f140b96dbd590b16935b93ab2f99b7b34658cd95
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-03-28 15:51:55 +0200

    Update do_calibration.py

--- dataReduction/do_calibration.py
+++ dataReduction/do_calibration.py
@@ -4,37 +4,45 @@ from glob import glob
 from fluxcal import fluxcal
 from collections import deque
 
-# inputfile_sci = 'path/to/raw/science/fits/file.fits'
-# inputfile_cal = 'path/to/raw/calibrator/fits/file.fits'
-# outputfile = 'path/to/calibrated/outputfile.fits'
-# cal_database_dir = 'path/to/calibrator/database/folder/'
-# cal_database_paths = [cal_database_dir+'vBoekelDatabase.fits',cal_database_dir+'calib_spec_db_v10.fits',cal_database_dir+'calib_spec_db_v10_supplement.fits']
-# output_fig_dir = 'path/to/figure/folder/'
-# fluxcal(inputfile_sci, inputfile_cal, outputfile, cal_database_paths, mode='corrflux',output_fig_dir=output_fig_dir)
-#
-# Arguments:
-# inputfile_sci: path to the raw science oifits file.
-# inputfile_cal: path to the raw calibrator oifits file.
-# outputfile: path of the output calibrated file
-# cal_database_paths: list of paths to the calibrator databases, e.g., [caldb1_path,caldb2_path]
-# mode (optional):
-#   'flux': calibrates total flux (incoherently processed oifits file expected)
-#           results written in the OI_FLUX table (FLUXDATA column)
-#    'corrflux': calibrates correlated flux (coherently processed oifits file expected)
-#                results written in the OI_VIS table (VISAMP column)
-#    'both': calibrates both total and correlated fluxes
-# output_fig_dir (optional): if it is a valid path, the script will make a plot of the calibrator model spectrum there,
-#                            deafult: '' (= no figure made)
+"""Wrapper for Jozsef Varga' script fluxcal"""
 
-# Datapath of the calib directories
+# Datapath of the calib directories, global variables
 CAL_DATABASE_DIR = os.path.join(os.getcwd(), "calib_spec_databases")
 CAL_DATABASE_FILES = ['vBoekelDatabase.fits', 'calib_spec_db_v10.fits',
                       'calib_spec_db_v10_supplement.fits']
 CAL_DATABASE_PATHS = [os.path.join(CAL_DATABASE_DIR, i) for i in CAL_DATABASE_FILES]
 
+def get_folder_path(dir_start: str, target: str,
+                    calibrator: str) -> str:
+    """Makes the folder names of the new folder for the calibrated files and
+    returns the 'output_dir'"""
+    working_dir = os.path.dirname(os.path.dirname(target))
+
+    # Formats the name of the new cal directory
+    dir_name, time_sci, band = os.path.dirname(target).split('.')[:-1]
+    dir_name = dir_name.split('/')[-1].replace("raw", "cal")
+    time_cal = os.path.dirname(calibrator).split('.')[-3]
+    new_dir_name = '.'.join([dir_start, dir_name, time_sci, band, time_cal, "rb"])
+
+    return os.path.join(working_dir, "calib", new_dir_name)
+
 def single_reduction(folder_dir_tar: str, folder_dir_cal: str,
                      mode: str) -> int:
-    """For documentation see 'do_reduction()'"""
+    """The calibration for a target and a calibrator folder
+
+    Parameters
+    ----------
+    base_path: str
+        The path to multiple folders that need to be cross correlated. Will be
+        skipped if folders for targets and calibrators are specified
+    folder_dir_tar: str, optional
+        A specific folder to be the target for calibration
+    folder_dir_cal: str, optional
+        A specific folder to be the calibrator for calibration
+    mode: str, optional
+        The mode of calibration. Either 'corrflux', 'flux' or 'both' depending
+        if it is 'coherent' or 'incoherent'. Default mode is 'corrflux'
+    """
     print(f"Calibrating {os.path.basename(folder_dir_tar)} with "\
           f"{os.path.basename(folder_dir_cal)}")
     dir_start = "TAR"
@@ -43,29 +51,26 @@ def single_reduction(folder_dir_tar: str, folder_dir_cal: str,
     if not targets:
         targets = glob(os.path.join(folder_dir_tar, "CALIB_RAW_INT*"))
         dir_start = "CAL"
+
     targets.sort(key=lambda x: x[-8:])
 
     calibrators = glob(os.path.join(folder_dir_cal, "CALIB_RAW_INT*"))
-    if not calibrators:
-        print("No 'CALIB_RAW_INT'-files found, Skipping!")
-        print("------------------------------------------------------------")
-        return -1
     calibrators.sort(key=lambda x: x[-8:])
     dir_start += "-CAL"
 
-    # Formats the name of the new cal directory
-    dir_name, time_sci, band = os.path.dirname(targets[0]).split('.')[:-1]
-    dir_name = dir_name.split('/')[-1].replace("raw", "cal")
-    time_cal = os.path.dirname(calibrators[0]).split('.')[-3]
-    new_dir_name = '.'.join([dir_start, dir_name, time_sci, band, time_cal, "rb"])
-
-    output_dir = os.path.join(base_path, "calib", new_dir_name)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    if not calibrators:
+        print("No 'CALIB_RAW_INT'-files found, probably target. Skipping!")
+        print("------------------------------------------------------------")
+        return -1
 
     if len(targets) != len(calibrators):
-        print("#'TARGET_RAW_INT'-files != #'CALIB_RAW_INT'-files, skipping!")
+        print("#'TARGET_RAW_INT'-files != #'CALIB_RAW_INT'-files. Skipping!")
+        print("------------------------------------------------------------")
         return -2
+
+    output_dir = get_folder_path(dir_start, targets[0], calibrators[0])
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     for i, o in enumerate(targets):
         print("------------------------------------------------------------")
@@ -74,9 +79,9 @@ def single_reduction(folder_dir_tar: str, folder_dir_cal: str,
         output_file = os.path.join(output_dir, f"TARGET_CAL_INT_000{i}.fits")
         fluxcal(o, calibrators[i], output_file,\
                 CAL_DATABASE_PATHS, mode=mode, output_fig_dir=output_dir)
+    print(f"Done calibrating {os.path.basename(folder_dir_tar)} with "\
+          f"{os.path.basename(folder_dir_cal)}")
     print("------------------------------------------------------------")
-    print("------------------------------------------------------------")
-
 
 def do_reduction(base_path: str, folder_dir_tar: str = None,
                  folder_dir_cal: str = None, mode="corrflux") -> None:
@@ -109,22 +114,44 @@ def do_reduction(base_path: str, folder_dir_tar: str = None,
                 if i != j:
                     single_reduction(i, j, mode)
 
-def do_full_reduction(folder: str) -> None:
+def do_full_reduction(folder: str, both: bool = True,
+                      lband: bool = False) -> None:
     """Does the full reduction for a full folder, being 'coherent',
-    'incoherent' and for these 'lband' and 'nband', respectively"""
+    'incoherent' and for these 'lband' and 'nband', respectively
+
+    Parameters
+    ----------
+    folder: str
+        The main folder to be calibrated
+    both: bool
+        If both nband and lband should be calibrated
+    lband: bool
+        If 'both=False' and this is True, then lband will be calibrated, if
+        'both=False" and this is False, then nband will be calibrated
+    """
     modes, bands = {"coherent": "corrflux", "incoherent": "flux"},\
             ["lband", "nband"]
 
-    for i, o in modes.items():
-        path = os.path.join(folder, i)
-        for j in bands:
-            temp_path = os.path.join(path, j)
-            print(f"Calibration of {temp_path}")
+    if both:
+        for i, o in modes.items():
+            path = os.path.join(folder, i)
+            for j in bands:
+                temp_path = os.path.join(path, j)
+                print(f"Calibration of {temp_path}")
+                print(f"with mode={o}")
+                print("------------------------------------------------------------")
+                do_reduction(temp_path, mode=o)
+    else:
+        for i, o in modes.items():
+            band = "lband" if lband else "nband"
+            path = os.path.join(folder, i, band)
+            print(f"Calibration of {path}")
             print(f"with mode={o}")
             print("------------------------------------------------------------")
-            do_reduction(temp_path, mode=o)
+            do_reduction(path, mode=o)
+
 
 if __name__ == "__main__":
     base_path = "/data/beegfs/astro-storage/groups/matisse/scheuck/data/GTO/hd142666/PRODUCTS/20190514"
-    do_full_reduction(base_path)
+    # do_full_reduction(base_path, both=True)
 
