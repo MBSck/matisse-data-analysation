Fix model scaling and work on mcmc

ppdmodler/src/functionality/baseClasses.py
@@ -26,7 +26,6 @@ class Model(metaclass=ABCMeta):
         self._radius, self._radius_range = [], []
         self._size, self._sampling = 0, 0
         self._axis_mod, self._axis_vis = [], []
-        self._pixel_scale_rad = 0.
 
     @property
     def size(self):
@@ -44,18 +43,7 @@ class Model(metaclass=ABCMeta):
     def axis_mod(self):
         return self._axis_mod
 
-    @property
-    def pixel_scale_rad(self):
-        return self._pixel_scale_rad
-
-    @pixel_scale_rad.setter
-    def pixel_scale_rad(self, obj_dimensions: float):
-        if self._sampling != 0.:
-            self._pixel_scale_rad = obj_dimensions/self._sampling
-        else:
-            self._pixel_scale_rad = obj_dimensions/self._size
-
-    def get_flux(self, optical_thickness: float,
+    def get_flux(self, pixel_scale: Union[int, float], optical_thickness: float,
                  q: float, T_sub: int, L_star: float,
                  distance: float, wavelength: float) -> np.array:
         """Calculates the total flux of the model
@@ -86,7 +74,7 @@ class Model(metaclass=ABCMeta):
         if self._radius_range:
             flux[self._radius_range] = 0.
 
-        flux *= (1-np.exp(-optical_thickness))*sr2mas(self._pixel_scale_rad)*1e26
+        flux *= (1-np.exp(-optical_thickness))*sr2mas(pixel_scale)*1e26
         flux[self._radius < r_sub] = 0.
         return np.sum(flux)
 

ppdmodler/src/functionality/utilities.py
@@ -97,12 +97,13 @@ def rad2mas(angle: Optional[float] = None):
     return 1/mas2rad(angle)
 
 def sr2mas(pixel_size: float):
-    """Converts the dimensions of an object from 'sr' to 'mas'
+    """Converts the dimensions of an object from 'sr' to 'mas'. the result is
+    in per pixel
 
     Parameters
     ----------
     pixel_size: float
-        The size of one pixel in rad
+        The size of one pixel in mas
     """
     return (pixel_size/(3600e3*180/np.pi))**2
 

ppdmodler/src/models/gauss2d.py
@@ -64,7 +64,6 @@ class Gauss2D(Model):
                                " the form [fwhm]")
 
         self._size, self._sampling = size, sampling
-        self.pixel_scale_rad = fwhm/2
         self._radius, self._axis_mod  = set_size(size, sampling, centre)
 
         return (1/np.sqrt(np.pi/(4*np.log(2)*fwhm)))*np.exp((-4*np.log(2)*self._radius**2)/fwhm**2)
@@ -109,8 +108,8 @@ class Gauss2D(Model):
 if __name__ == "__main__":
     g = Gauss2D()
     g_model = g.eval_model([5.3], size:=256)
-    g_flux = g.get_flux(1, 0.55, 1500, 19, 140, 9e-6)
-    print(g_flux[size//2, size//2])
+    g_flux = g.get_flux(10, 1, 0.55, 1500, 19, 140, 9e-6)
+    print(g_flux)
     plt.imshow(g_model)
     plt.show()
 