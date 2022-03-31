0521e71ec72dab6e939dee523b9da5a32025cdb0
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-03-31 14:01:41 +0200

    Create do_merge.py

---
+++ dataReduction/do_merge.py
@@ -0,0 +1,11 @@
+from avg_oifits import oifits_patchwork
+
+
+
+if __name__ == "__main__":
+    ...
+    coherent_file = ""
+    incoherent_file = ""
+    outfile_path = ""
+    oifits_patchwork([coherent_file, incoherent_file], outfile_path)
+
