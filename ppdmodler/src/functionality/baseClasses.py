Work on fix of scaling of FFT

ppdmodler/src/functionality/baseClasses.py
@@ -29,6 +29,7 @@ class Model(metaclass=ABCMeta):
         self._size, self._sampling, self._mas_size = 0, 0, 0
         self._axis_mod, self._axis_vis = [], []
         self._phi = []
+        self._inner_r = 0
 
         self.T_sub, self.T_eff, self.L_star, self.d, self.wl = T_sub, T_eff, \
                 L_star, distance, wavelength
@@ -63,7 +64,11 @@ class Model(metaclass=ABCMeta):
         -------
         flux: np.ndarray
         """
-        T = temperature_gradient(self._radius, self._r_sub, q, self.T_sub)
+        if self._inner_r != 0:
+            T = temperature_gradient(self._radius, self._inner_r, q, self.T_sub)
+        else:
+            T = temperature_gradient(self._radius, self._r_sub, q, self.T_sub)
+
         flux = plancks_law_nu(T, self.wl)
         flux *= (1-np.exp(-optical_thickness))*sr2mas(self._mas_size, self._sampling)
         flux[np.where(np.isnan(flux))], flux[np.where(np.isinf(flux))] = 0., 0.

ppdmodler/src/functionality/fourier.py
@@ -8,12 +8,13 @@ import time
 import matplotlib.pyplot as plt
 
 from numpy import fft
+from scipy import interpolate
 from pathlib import Path
 from typing import Any, List, Dict, Optional, Union
 
 # Own modules
-from src.models import Gauss2D, Ring, InclinedDisk
-from src.functionality.utilities import timeit, get_px_scaling, zoom_array
+from src.functionality.utilities import timeit, get_px_scaling, zoom_array,\
+        mas2rad
 
 # TODO: Make class and function documentation
 
@@ -28,10 +29,29 @@ class FFT:
     def __init__(self, model: np.array, wavelength: float, set_size: int = None,
                  pixelscale: float = 1.) -> None:
         self.model = model
-        self.model_size = len(self.model)
+        s = model.shape
+        self.dim = s[0]
+        self.zpfact = 1
         self.set_size = set_size
+        self.px_size = 0.1                  # [mas]
 
-        self.fftfreq = fft.fftfreq(self.model_size, d=pixelscale)
+    @property
+    def fftfreq(self):
+        return np.roll(fft.fftfreq(self.dim*self.zpfact,
+                                   mas2rad(self.px_size)), self.dim//2)
+
+    @property
+    def fftscaling_mlambda(self):
+        return np.roll(fft.fftfreq(self.dim*self.zpfact,
+                                   mas2rad(self.px_size)), self.dim//2)*1e6
+
+    def interpolate_uv2fft2(self, uvcoords: np.ndarray):
+        grid = (self.fftfreq, self.fftfreq)
+        real=interpolate.interpn(grid, np.real(fft2D), uvcoords, method='linear',
+                                 bounds_error=False, fill_value=None)
+        imag=interpolate.interpn(grid, np.imag(fft2D), uvcoords, method='linear',
+                                 bounds_error=False, fill_value=None)
+        return real+imag*1j
 
     def pipeline(self, zoom: bool = False) -> [np.ndarray, np.ndarray, np.ndarray]:
         """Combines various functions and executes them
@@ -66,7 +86,10 @@ class FFT:
 
         return ft, amp, phase
 
-    @timeit
+    def ft_translate(self, uvcoord, vcoord, wavelength, x=0, y=0):
+        """The translation factor for the fft"""
+        return np.exp(-2j*np.pi*(uvcoord*x+vcoord*y))
+
     def do_fft2(self) -> np.array:
         """Does the 2D-FFT and returns the 2D-FFT and shifts the centre to the
         middle
@@ -76,10 +99,10 @@ class FFT:
         ft: np.ndarray
         ft_raw: np.ndarray
         """
-        return fft.fftshift(fft.fft2(fft.ifftshift(self.model))), \
-                fft.fft2(self.model)
+        s = [self.zpfact*self.dim, self.zpfact*self.dim]
+        return fft.fftshift(fft.fft2(fft.fftshift(self.model), s=s)), \
+                fft.fft2(self.model, s=s)
 
-    @timeit
     def do_ifft2(self) -> np.array:
         """Does the inverse 2D-FFT and shifts the centre to the middle
 

ppdmodler/src/functionality/submain.py
@@ -11,60 +11,17 @@ from src.functionality.fourier import FFT
 # Shows the full np.arrays, takes ages to print the arrays
 # np.set_printoptions(threshold=sys.maxsize)
 
-def comparefft2modvis(model, wavelength):
-    model_mod = model.eval_model(128, 10.)
-    model_vis = model.eval_vis(128, 10., wavelength)
-    ft = FFT(model_mod, 0, wavelength).do_fft2()
-    return [np.mean(np.array([[ np.linalg.norm(i-j) for j in model_vis[x]] for i in ft[x]])) for x in range(128)], \
-            (np.mean(np.abs(model_vis)), np.mean(np.abs(ft)))
 
 def main():
     fig, (ax1, ax2) = plt.subplots(1, 2)
     wavelength = 8e-6
-    cp = CompoundModel()
-    cp_model = cp.eval_model([130.15469669, 140.19231673, 83.54664411, 0.17167066, 4.2379771], 128)
+    cp = CompoundModel(1500, 7900, 19, 140, wavelength)
+    cp_model = cp.eval_model([1, 45], 5, 128)
     ax1.imshow(cp_model)
 
-    ft, amp, phase = FFT(cp_model, wavelength).pipeline(vis2=True)
-    amp = amp*np.conj(amp)
+    ft, amp, phase = FFT(cp_model, wavelength).pipeline()
     ax2.imshow(abs(amp))
     plt.show()
-
-    '''
-    file = "/Users/scheuck/Documents/PhD/matisse_stuff/ppdmodler/assets/TARGET_CAL_INT_0001bcd_calibratedTEST.fits"
-    readout = ReadoutFits(file)
-    wavelength = readout.get_wl()[80]
-    print(wavelength)
-    print(readout.get_vis24wl(80))
-    uvcoords = readout.get_uvcoords()
-    print(uvcoords)
-
-    model = Gauss2D()
-    model_vis  = model.eval_vis(128, 35.5124433, wavelength, None)
-    print(model_vis)
-    model_axis_vis = model.axis_vis
-    uvcoords, uv_ind = correspond_uv2model(model_vis, model_axis_vis, uvcoords, dis=True)
-    print(uvcoords, "The corresponded vis2data")
-    # plt.imshow(model_vis)
-    # plt.show()
-    '''
-
-    '''
-    # Set up the parameters of the problem.
-    ndim, nsamples = 3, 50000
-
-    # Generate some fake data.
-    np.random.seed(42)
-    data1 = np.random.randn(ndim * 4 * nsamples // 5).reshape([4 * nsamples // 5, ndim])
-    data2 = (4*np.random.rand(ndim)[None, :] + np.random.randn(ndim * nsamples // 5).reshape([nsamples // 5, ndim]))
-    data = np.vstack([data1, data2])
-
-    # Plot it.
-    fig =  corner.corner(data, labels=[r"$x$", r"$y$", r"$\log \alpha$", r"$\Gamma \, [\mathrm{parsec}]$"],
-                           quantiles=[0.16, 0.5, 0.84],
-                           show_titles=True, title_kwargs={"fontsize": 12})
-    plt.show()
-    '''
 
 if __name__ == "__main__":
     # print(comparefft2modvis(Gauss2D(), 8e-06))

ppdmodler/src/functionality/utilities.py
@@ -121,8 +121,7 @@ def sr2mas(mas_size: float, sampling: int):
     """
     return (mas_size/(sampling*3600e3*180/np.pi))**2
 
-def get_px_scaling(ax: np.ndarray, wavelength: float,
-                   mas_size: int, px_size: int) -> float:
+def get_px_scaling(ax: np.ndarray, wavelength: float) -> float:
     """Gets the model's scaling from its sampling rate/size the wavelength and
     the array's dimensionalities into 1/radians.
 
@@ -137,8 +136,7 @@ def get_px_scaling(ax: np.ndarray, wavelength: float,
     float
         The px to meter scaling
     """
-    axis = np.roll(ax, (axis_size := len(ax))//2, 0)
-    return np.diff(ax)[0]*wavelength/(mas2rad()*(mas_size/px_size))
+    return np.diff(ax)[0]*wavelength
 
 def correspond_uv2scale(scaling: float, roll: int, uvcoords: np.ndarray) -> float:
     """Calculates the axis scaling from the axis of an input image/model and
@@ -272,7 +270,6 @@ def set_size(mas_size: int, px_size: int, sampling: Optional[int] = 0,
 
         xr, yr = x*np.sin(pos_angle)+y*np.cos(pos_angle), \
                 (x*np.cos(pos_angle)-y*np.sin(pos_angle))/axis_ratio
-        print(xr, yr)
         radius = np.sqrt(xr**2+yr**2)
         axis, phi = [xr, yr], np.arctan2(xr, yr)
     else:

ppdmodler/src/models/compound_model_fast.py
@@ -40,7 +40,7 @@ class CompoundModel(Model):
 
     @timeit
     def eval_model(self, theta: List, mas_size: int, px_size: int,
-                   sampling: Optional[int] = None) -> np.ndarray:
+                   sampling: Optional[int] = None, inner_radius = 0) -> np.ndarray:
         """Evaluates the model. In case of zero divison error, the major will be replaced by 1
 
         Returns
@@ -63,7 +63,9 @@ class CompoundModel(Model):
 
         self._size, self._mas_size = px_size, mas_size
 
-        image = self.r.eval_model([axis_ratio, pa], mas_size, px_size, sampling)
+        image = self.r.eval_model([axis_ratio, pa], mas_size, px_size,
+                                  sampling, inner_radius)
+        self._max_obj = np.max(image)
         image += self.d.eval_model(mas_size, px_size)
         return image
 

ppdmodler/src/models/gauss2d.py
@@ -7,7 +7,9 @@ from typing import Any, Dict, List, Union, Optional
 
 from src.functionality.baseClasses import Model
 from src.functionality.utilities import timeit, set_size, set_uvcoords,\
-        mas2rad
+        mas2rad, get_px_scaling
+
+from src.functionality.fourier import FFT
 
 class Gauss2D(Model):
     """Two dimensional Gauss model, FFT is also Gauss
@@ -23,8 +25,8 @@ class Gauss2D(Model):
     eval_vis2():
         Evaluates the visibilities of the model
     """
-    def __init__(self):
-        super().__init__()
+    def __init__(self, T_sub, T_eff, L_star, distance, wavelength):
+        super().__init__(T_sub, T_eff, L_star, distance, wavelength)
         self.name = "2D-Gaussian"
 
     @timeit
@@ -95,23 +97,47 @@ class Gauss2D(Model):
         set_uvcoords()
         """
         try:
-            fwhm = theta[0]
+            fwhm = mas2rad(theta[0])
         except:
             raise RuntimeError(f"{self.name}.{inspect.stack()[0][3]}():"
                                " Check input arguments, theta must be"
                                " of the form [fwhm]")
 
         self._sampling = sampling
-        B, self._axis_vis  = set_uvcoords(sampling, wavelength, uvcoords=uvcoords)
+        B, self._axis_vis  = set_uvcoords(sampling, wavelength=wavelength, uvcoords=uvcoords)
 
         return np.exp(-(np.pi*fwhm*B)**2/(4*np.log(2)))
 
 if __name__ == "__main__":
-    g = Gauss2D()
-    g_model = g.eval_model([2], 10, 256)
-    print(g_flux := g.get_flux(0.5, 0.55, 1500, 19, 140, 9e-6))
-    # plt.imshow(g._radius)
-    plt.imshow(g_model)
-    # plt.plot(np.linspace(0, 256, 256), g_flux[128])
+    g = Gauss2D(1500, 7900, 19, 140, wavelength:=8e-6)
+    g_model = g.eval_model([2.5], mas_fov:=10, sampling:=129)
+    g_flux = g.get_flux(np.inf, 0.7)
+    g_tot_flux = g.get_total_flux(np.inf, 0.7)
+    g_vis = g.eval_vis([2.5], sampling, wavelength)
+    fig, (ax, bx, cx, dx) = plt.subplots(1, 4, figsize=(20, 5))
+    fft = FFT(g_model, wavelength)
+    ft, amp2, phase = fft.pipeline()
+    ft_scaling = get_px_scaling(fft.fftfreq, wavelength)
+    print(fft.fftfreq)
+    print(ft_scaling)
+    print(ft_ax := ft_scaling//2*sampling)
+    ax.imshow(g_model, extent=[mas_fov, -mas_fov, -mas_fov, mas_fov])
+    bx.imshow(g_flux, extent=[mas_fov, -mas_fov, -mas_fov, mas_fov])
+    cx.imshow(amp2, extent=[ft_ax, -ft_ax, -ft_ax, ft_ax])
+    dx.imshow(amp2.copy()[61:68, 61:68], extent=[550, -550, -550, 550])
+
+    ax.set_title("Model image, Object plane")
+    bx.set_title("Temperature gradient")
+    cx.set_title("Fourier transform of object plane (normed). vis")
+    dx.set_title("Fourier transform of object plane (normed, zoomed). vis")
+
+    ax.set_xlabel("RA [mas]")
+    ax.set_ylabel("DEC [mas]")
+    bx.set_xlabel("RA [mas]")
+    bx.set_ylabel("DEC [mas]")
+    cx.set_xlabel("u [m]")
+    cx.set_ylabel("v [m]")
+    dx.set_xlabel("u [m]")
+    dx.set_ylabel("v [m]")
     plt.show()
 

ppdmodler/src/models/ring.py
@@ -8,8 +8,9 @@ from typing import Any, Dict, List, Union, Optional
 
 from src.functionality.baseClasses import Model
 from src.functionality.constant import I
+from src.functionality.fourier import FFT
 from src.functionality.utilities import timeit, set_size, set_uvcoords,\
-        delta_fct, mas2rad, trunc, azimuthal_modulation
+        delta_fct, mas2rad, trunc, azimuthal_modulation, get_px_scaling
 
 # TODO: Make the addition of the visibilities work properly, think of OOP
 # abilities
@@ -39,7 +40,8 @@ class Ring(Model):
     @timeit
     def eval_model(self, theta: List, mas_size: int, px_size: int,
                    sampling: Optional[int] = None,
-                   inner_radius: Optional[int] = None) -> np.array:
+                   inner_radius: Optional[int] = 0,
+                   outer_radius: Optional[int] = 0) -> np.array:
         """Evaluates the model
 
         Parameters
@@ -72,6 +74,7 @@ class Ring(Model):
                                " Check input arguments, theta must be of"
                                " the form [axis_ratio, pos_angle]")
 
+        self._inner_r = inner_radius
         if sampling is None:
             self._sampling = sampling = px_size
         else:
@@ -86,6 +89,9 @@ class Ring(Model):
             radius[radius < inner_radius] = 0.
         else:
             radius[radius < self._r_sub] = 0.
+
+        if outer_radius:
+            radius[radius > outer_radius] = 0.
 
         self._radius = radius.copy()
         radius[np.where(radius != 0)] = 1/(2*np.pi*self._r_sub)
@@ -153,10 +159,38 @@ class Ring(Model):
 
 
 if __name__ == "__main__":
-    r = Ring(1500, 7900, 19, 140, 8e-6)
-    r_model = r.eval_model([5.2, 3.47494126e+02], 30, 2048)
-    print(r_flux := r.get_flux(0.2, 0.7))
-    print(r.get_total_flux(0.2, 0.7))
-    plt.imshow(r_flux)
+    wavelength = 8e-6
+    r = Ring(1500, 7900, 19, 140, 10e-6)
+    r_model = r.eval_model([1, 140], mas_fov:=30, sampling:=129,\
+                           outer_radius=1.10*r._r_sub)
+    r_flux = r.get_flux(np.inf, 0.7)
+    r_tot_flux = r.get_total_flux(np.inf, 0.7)
+    fig, (ax, bx, cx, dx) = plt.subplots(1, 4, figsize=(20, 5))
+    fft = FFT(r_model, wavelength)
+    ft, amp2, phase = fft.pipeline()
+    ft_scaling = get_px_scaling(fft.fftfreq, wavelength)
+    print(r._r_sub)
+    print(fft.fftfreq)
+    print(ft_scaling)
+    print(ft_ax := ft_scaling//2*sampling)
+    ft_lambda = ft_ax/(wavelength*1e6)
+    ax.imshow(r_model, extent=[mas_fov, -mas_fov, -mas_fov, mas_fov])
+    bx.imshow(r_flux, extent=[mas_fov, -mas_fov, -mas_fov, mas_fov])
+    cx.imshow(amp2, extent=[ft_ax, -ft_ax, -ft_ax, ft_ax])
+    dx.imshow(amp2, extent=[ft_lambda, -ft_lambda, -ft_lambda, ft_lambda])
+
+    ax.set_title("Model image, Object plane")
+    bx.set_title("Temperature gradient")
+    cx.set_title("Fourier transform of object plane (normed). vis")
+    dx.set_title("Fourier transform of object plane (normed, zoomed). vis")
+
+    ax.set_xlabel("RA [mas]")
+    ax.set_ylabel("DEC [mas]")
+    bx.set_xlabel("RA [mas]")
+    bx.set_ylabel("DEC [mas]")
+    cx.set_xlabel("u [m]")
+    cx.set_ylabel("v [m]")
+    dx.set_xlabel("u [m]")
+    dx.set_ylabel("v [m]")
     plt.show()
 

ppdmodler/src/models/submain.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 from src.functionality.readout import ReadoutFits
 from src.functionality.fourier import FFT
-from src.functionality.utilities import trunc, azimuthal_modulation
+from src.functionality.utilities import trunc, azimuthal_modulation, \
+        get_px_scaling
 from src.models import Gauss2D, Ring, CompoundModel, InclinedDisk
 
 # Shows the full np.arrays, takes ages to print the arrays
@@ -26,32 +27,57 @@ def check_flux_behaviour(model, wavelength):
         print("|| ", o[0], ": ", trunc(o[1], 3),
               " || ", lst[~i][0], ": ", trunc(lst[~i][1], 3), " ||")
 
-def plot_all(model, mas_size, wavelength):
-    mod = model.eval_model([35.57632429, 73.21246517, 81.30680296], mas_size, 1024)
-    flux = model.get_flux(0.50409545, 0.3649148)
-    tot_flux = model.get_total_flux(0.5, 0.55)
-    print(tot_flux)
+def plot_all(model, mas_size, wavelength, sampling):
+    mod = model.eval_model([1, 140], mas_size, sampling)
+    flux = model.get_flux(np.inf, 1)
+    tot_flux = model.get_total_flux(np.inf, 1)
+    print(tot_flux, "total flux")
 
-    fourier = FFT(mod, wavelength)
+    fourier = FFT(mod, wavelength, pixelscale=1.)
     ft, amp, phi = fourier.pipeline()
+
+    fr_scale = get_px_scaling(fourier.fftfreq, wavelength)
+    print(fourier.fftfreq, "scaling")
+    print(fr_scale, "fr_scale")
+    corr_scale = fr_scale*sampling/2
+    print(corr_scale, "u_min")
+    u, v = (axis := np.linspace(-corr_scale, corr_scale, sampling)),\
+            axis[:, np.newaxis]
     corr_flux = amp*tot_flux
 
-    u, v = (axis := np.linspace(-150, 150, 1024)), axis[:, np.newaxis]
+
+    # u, v = (axis := np.linspace(-150, 150, sampling)), axis[:, np.newaxis]
     wavelength = trunc(wavelength*1e06, 2)
-    xvis_curve = np.sqrt(u**2+v**2)[centre := 1024//2]
-    yvis_curve = corr_flux[centre]
+    xvis_curve = np.sqrt(u**2+v**2)[centre := sampling//2]
+    yvis_curve = amp[centre]
+    yvis_curve2 = corr_flux[centre]
+
+    third_max = len(corr_flux)//2 - 3
 
     fig, (ax, bx, cx, dx, ex) = plt.subplots(1, 5)
-    ax.imshow(mod, extent=[mas_size/2, -mas_size/2, -mas_size/2, mas_size/2])
-    bx.imshow(flux, extent=[mas_size/2, -mas_size/2, -mas_size/2, mas_size/2])
-    cx.imshow(corr_flux, extent=[mas_size/2, -mas_size/2, -mas_size/2, mas_size/2])
-    dx.plot(xvis_curve, yvis_curve)
-    ex.imshow(np.log(amp*tot_flux), extent=[mas_size/2, -mas_size/2, -mas_size/2, mas_size/2])
+    ax.imshow(mod, vmax=model._max_obj,
+              extent=[mas_size/2, -mas_size/2, -mas_size/2, mas_size/2])
+    bx.imshow(flux, vmax=model._max_sub_flux, extent=[mas_size/2, -mas_size/2, -mas_size/2, mas_size/2])
+    cx.plot(xvis_curve, yvis_curve)
+    dx.plot(xvis_curve, yvis_curve2)
+    ex.imshow(corr_flux, vmax=corr_flux[third_max, third_max],
+             extent=[corr_scale, -corr_scale, -corr_scale, corr_scale])
+
     ax.set_title("Object Plane")
     bx.set_title("Temperature gradient")
-    cx.set_title("Correlated Fluxes (vis*tot_flux)")
+    cx.set_title("Visibilities")
+    dx.set_title("Correlated Fluxes")
+    ex.set_title("Correlated Fluxes")
+    ax.set_xlabel("[mas]")
+    ax.set_ylabel("[mas]")
+    bx.set_xlabel("[mas]")
+    bx.set_ylabel("[mas]")
+    cx.set_xlabel(r"$B_p$ [m]")
+    cx.set_ylabel(r"vis")
     dx.set_xlabel(r"$B_p$ [m]")
-    ex.set_title("Correlated Fluxes (vis*tot_flux) [ln]")
+    dx.set_ylabel(r"Corr Flux [Jy]")
+    ex.set_xlabel(r"$u$ [m]")
+    ex.set_ylabel(r"$v$ [m]")
     plt.show()
 
 def main():
@@ -59,8 +85,9 @@ def main():
 
     # How to use corr_flux with vis in model
     c = CompoundModel(1500, 7900, 19, 140, wavelength)
-    plot_all(c, 50, wavelength)
+    plot_all(c, 10, wavelength, 2048)
 
 if __name__ == "__main__":
     # print(comparefft2modvis(Gauss2D(), 8e-06))
     main()
+