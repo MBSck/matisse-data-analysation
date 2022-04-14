4ef7e4ff98875f1a385c460912dcaf63a0060cae
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-04-14 10:52:46 +0200

    Update parseOBplan.py

--- observation/phase2/parseOBplan.py
+++ observation/phase2/parseOBplan.py
@@ -16,8 +16,9 @@ def save_night_lst(input_dict: List, output_path: str):
     with open(output_path, "wb") as fp:
         pickle.dump(input_dict, fp)
 
-def get_nights(file: Path, run_identifier: str = "Night",
-               default_key: str = "full_night") -> Dict[str, List]:
+def get_nights(file: Path, run_identifier: Optional[str] = "run",
+               sub_identifier: Optional[str] = "night",
+               default_key: Optional[str] = "full_run") -> Dict[str, List]:
     """
     Parses the night plan created with 'calibrator_find.pro' into the
     individual runs as key of a dictionary, specified by the 'run_identifier'.
@@ -31,9 +32,12 @@ def get_nights(file: Path, run_identifier: str = "Night",
     run_identifier: str, optional
         Set to default identifier that splits the individual runs into keys of
         the return dict as 'run'
+    sub_identifier: str, optional
+        Set to default sub identifier that splits the individual runs into the
+        individual nights. That is, in keys of the return dict as 'night'
     default_key: str, optional
         The default identifier that is choosen as a key if 'run_identifier' has
-        no matches. Defaults to 'full_night'
+        no matches. Defaults to 'full_run'
 
     Returns
     -------
@@ -42,40 +46,45 @@ def get_nights(file: Path, run_identifier: str = "Night",
         containing the sub lists 'sci_lst', 'cal_lst' and 'tag_lst'
     """
     lines = readout_txt(file)
-    run_indices_lst = [i for i, o in  enumerate(lines) if run_identifier in o]
-    run_labels = [i.replace('\n', '') for i in lines if run_identifier in i]
+    run_indices_lst, run_labels = [], []
+    for i, o in enumerate(lines):
+        if run_identifier in o.lower():
+            run_indices_lst.append(i)
+            run_labels.append(o.replace('\n', ''))
 
-    # TODO: Enable this for bigger night plans that also contain the night and
-    # the run as an identifier
-
-    # Debug, remove
-    print(run_indices_lst)
-    print(run_labels)
+    runs = [lines[o:] if o == run_indices_lst[~0] else \
+                  lines[o:run_indices_lst[i+1]] for i, o in enumerate(run_indices_lst)]
 
     # If there are no specified search terms, whole list will be parsed
     if len(run_indices_lst) == 0:
         run_indices_lst, run_labels = [1], [default_key]
 
+    night_indices_lst, night_labels = [], []
+    for i, o in enumerate(runs):
+        night_indices_lst.append([])
+        night_labels.append([])
+        for j, l in enumerate(o):
+            if sub_identifier in l.lower():
+                night_indices_lst[i].append(j)
+                night_labels[i].append(l)
+
+    nights = []
+    for i, o in enumerate(runs):
+        night = [o[j:] if l == night_indices_lst[i][~0] else\
+                 o[j:night_indices_lst[i][j+1]] for j, l in enumerate(night_indices_lst[i])]
+        nights.append(night)
+
     # Only gets the lines of the runs starting with numbers (SCI or CAL)
-    runs = [lines[o:] if o == run_indices_lst[~0] else \
-                  lines[o:run_indices_lst[i+1]] for i, o in enumerate(run_indices_lst)]
     runs_cal_sci = [[j for j in i if j[0] in ["1", "0"]] for i in runs]
 
     runs_dict = {}
-    # runs_dict2 = {for i in runs_cal_sci[0]}
-
-    # Gets the sci-, cal-, and ln-list for the nights
     for i, o in enumerate(runs_cal_sci):
         sci_lst, cal_lst, tag_lst = [], [], []
         counter = -1
         for j in o:
-            # sci_lst = [x[1]+x[2] for x in o if not "cal" in j[1]]
             j = j.split(" ")
-            print(j)
-            # cal_lst = [x for x in j if "cal" in j[1]]
             if "cal" in j[1]:
                 temp_cal = j[1].split("_")
-                print(temp_cal)
                 tag_lst[counter].append(temp_cal[1])
                 cal_lst[counter].append(temp_cal[2])
             else:
@@ -84,15 +93,12 @@ def get_nights(file: Path, run_identifier: str = "Night",
                 tag_lst.append([])
                 sci_lst.append(j[1]+j[2])
         runs_dict[run_labels[i]] = [sci_lst, cal_lst, tag_lst]
-    print(runs_dict)
-    runs_dict = {}
-    print(runs_dict)
 
     return runs_dict
 
 
 if __name__ == "__main__":
-    path = "/Users/scheuck/Documents/PhD/matisse_stuff/observation/P108/march2022/p108_MATISSE_YSO_runs_observing_plan_v0.1.txt"
+    path = "/Users/scheuck/Documents/PhD/matisse_stuff/observation/P109/april2022/p109_MATISSE_YSO_runs_observing_plan_v0.1.txt"
     print(path)
     nights = get_nights(path)
     # save_night_lst()
