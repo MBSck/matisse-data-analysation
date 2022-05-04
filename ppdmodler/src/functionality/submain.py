c7242a54b11741109d9f99e2209d964b17442b43
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-05-04 12:18:59 +0200

    Update submain.py
    Fit test of the genetic fit algorithm

--- ppdmodler/src/functionality/submain.py
+++ ppdmodler/src/functionality/submain.py
@@ -5,6 +5,7 @@ import corner
 
 from src.functionality.readout import ReadoutFits
 from src.functionality.utilities import set_uvcoords, correspond_uv2model
+from src.functionality.genetic_algorithm import genetic_algorithm, decode
 from src.models import Gauss2D, CompoundModel
 from src.functionality.fourier import FFT
 
@@ -43,6 +44,27 @@ def main():
 
     plt.show()
 
+def straigth(x):
+    xcoords = np.linspace(0, 100)
+    return xcoords, xcoords*x[0] + x[1]
+
+def chi_sq(x):
+    return np.sum((data - straigth(x)[1])**2/(data*0.2)**2)
+
+def test_chi_sq():
+    bounds = [[0., 1.], [0, 5]]
+    n_bits = 32
+    n_pop, n_iter = 100, 1000
+    r_cross = 0.85
+    best, scores = genetic_algorithm(chi_sq, bounds, n_bits, n_iter, n_pop,
+                                     r_cross, 1./(n_bits*len(bounds)))
+    decoded = decode(bounds, n_bits, best)
+    return decoded
+
 if __name__ == "__main__":
-    main()
+    data = straigth((0.5, 2))[1]
+    print(decoded := test_chi_sq())
+    plt.plot(*straigth((0.5, 2)))
+    plt.plot(*straigth(decoded))
+    plt.show()
 
