54ea56b9b4e22d0e26b803c8ce9be90cbf648b4b
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-05-16 14:15:31 +0200

    Update automatedOBcreation.py
    Add more comments, implement resolution dicts, implement reading out of resolution dicts

--- observation/phase2/automatedOBcreation.py
+++ observation/phase2/automatedOBcreation.py
@@ -1,4 +1,53 @@
+#!/usr/bin/env python3
+
+"""Automated OB Creation
+
+...
+
+This file can also be imported as a module and contains the following functions:
+
+    * load_yaml -
+    * set_res -
+    * make_sci_obs -
+    * make_cal_obs -
+    * ob_pipeline -
+
+Example of usage:
+    >>> from automatedOBcreation import ob_pipeline
+
+    # Script needs either manual_lst or path2file (to the parsed night plan) in
+    # (.yaml)-file format
+
+    >>> path2file = "night_plan.yaml"
+
+    # Calibration lists accept sublists (with more than one item)
+    # but also single items, same for tag_lst
+
+    >>> sci_lst = ["AS 209", "VV Ser"]
+    >>> cal_lst = [["HD 142567", "HD 467893"], "]
+    >>> tag_lst = [["LN", "N"], "L"]
+
+    # Specifies the paths, where the '.obx'-files are saved to, name of run can
+    # be changed to actual one
+
+    >>> outdir = "..."
+
+    # Specifies the res_dict. Can be left empty. Changes only L-band res atm
+
+    >>> res_dict = {"AS 209": "MR"}
+
+    # Templates that can be used, for acq. only one at a time, for obs multiple
+    # For instance ob.acq_ft_tpl, ob.acq_tpl, ob.obs_tpl, ob.obs_ft_tpl,
+    # ob.obs_ft_coh_tpl, ob.obs_ft_vis_tpl
+
+    >>> ob_pipeline("UTs", outpath, path2file,
+                    manual_lst=[sci_lst, cal_lst, tag_lst],
+                    res_dict=res_dict, standard_res=["LR", "LR"],
+                    obs_templates=[ob.obs_tpl], acq_template=ob.acq_tpl)
+"""
+
 __author__ = "Marten Scheuck"
+__date__   = "2022-05-12"
 
 import os
 import yaml
@@ -8,63 +57,44 @@ from pathlib import Path
 from types import SimpleNamespace
 from typing import Any, Dict, List, Union, Optional
 
-import parseOBplan
 import MATISSE_create_OB_2 as ob
 
-# TODO: Implement this for MATISSE standalone as well
-# TODO: Fix the fact that happens when a calibrator is first before the SCI
-# TODO: Implement various setups, long lists for templates etc.
 # TODO: Make this work for n-band as well
-# TODO: High res cal file is erroneous
-# TODO: Make standard setting for resolution possible
-# TODO: Make setting both resolutions possible
-# TODO: Set dit to 0.111 for standalone matisse
 # TODO: Reset all changes after the OB creation
+
+# Dicts for the template and resolution configuration
+
+UT_DICT_STANDALONE = {"ACQ": ob.acq_tpl,
+                      "LOW": {"TEMP": [ob.obs_tpl], "DIT": [0.111],
+                              "RES": "L-LR_N-LR"}}
+
+AT_DICT_GRA4MAT = {"ACQ": ob.acq_ft_tpl,
+                   "LOW": {"TEMP": [ob.obs_ft_tpl], "DIT": [1.], "RES":
+                           "L-LR_N-LR"},
+                   "MED": {"TEMP": [ob.obs_ft_tpl],
+                           "DIT": [1.3], "RES": "L-MR-LR"},
+                   "HIGH": {"TEMP": [ob.obs_ft_tpl],
+                           "DIT": [3.], "RES": "L-HR-LR"}}
+
+UT_DICT_GRA4MAT = {"ACQ": ob.acq_ft_tpl,
+                   "LOW": {"TEMP": [ob.obs_ft_vis_tpl], "DIT": [0.111], "RES":
+                           "L-LR_N-LR"},
+                   "MED": {"TEMP": [ob.obs_ft_coh_tpl, ob.obs_ft_vis_tpl],
+                           "DIT": [1.3, 0.111], "RES": "L-MR-LR"},
+                   "HIGH": {"TEMP": [ob.obs_ft_coh_tpl, ob.obs_ft_vis_tpl],
+                           "DIT": [3., 0.111], "RES": "L-HR-LR"}}
+
+TEMPLATE_DICT = {"standalone": {"UTs": UT_DICT_STANDALONE},
+                 "GRA4MAT": {"UTs": UT_DICT_GRA4MAT, "ATs": AT_DICT_GRA4MAT}}
 
 def load_yaml(file_path):
     """Loads a '.yaml'-file into a dictionary"""
     with open(file_path, "r") as fy:
         return yaml.safe_load(fy)
 
-def set_res(standard_res: List):
-    """placeholder function until setting both res is understood with josef's
-    code"""
-    # TODO: Remove this and make it work with get_res_dit function for all of
-    # the code
-    dit = 1.
-
-    if "MR" in standard_res:
-        dit = 1.3
-    if "HR" in standard_res:
-        dit = 3.
-
-    return f"L-{standard_res[0]}_N-{standard_res[1]}", dit
-
-def get_res_dit(resolution: str):
-    """Gets the dit for the resolution input
-
-    Parameters
-    ----------
-    resolution: str
-        In either 'MR' or 'HR'
-
-    Returns
-    -------
-    resolution: str
-        In the right input form for the 'make_cal_obs'
-    dit: float
-        The corresponding dit to the resolution
-    """
-    # NOTE: Remove this as well
-    if "MR" in resolution:
-        return "L-MR_N-LR", 1.3
-    if "HR" in resolution:
-        return "L-HR_N-LR", 3.
-    return  "L-LR_N-LR", 1.
-
 def make_sci_obs(sci_lst: List, array_config: str,
                  outdir: str, res_dict: Dict, standard_res: List,
-                 obs_templates: List, acq_template) -> None:
+                 obs_templates: List, acq_template, mode: str) -> None:
     """Gets the inputs from a list and calls the 'mat_gen_ob' for every list element
 
     Parameters
@@ -86,12 +116,13 @@ def make_sci_obs(sci_lst: List, array_config: str,
     None
     """
     try:
+        array_key = "UTs" if array_config == "UTs" else "ATs"
+
         for i in sci_lst:
             if i in res_dict:
                 resolution, dit = get_res_dit(res_dict[i])
             else:
                 resolution, dit = set_res(standard_res)
-            dit = [0.111, 1.3]
 
             ob.mat_gen_ob(i, array_config, 'SCI', outdir=outdir,\
                           spectral_setups=[resolution], obs_tpls=obs_templates,\
@@ -131,16 +162,16 @@ def make_cal_obs(cal_lst: List, sci_lst: List, tag_lst: List,
     None
     """
     try:
-        # Iterates through the calibration list
+        array_key = "UTs" if array_config == "UTs" else "ATs"
+
+        # NOTE: Iterates through the calibration list
         for i, o in enumerate(cal_lst):
             if sci_lst[i] in res_dict:
                 resolution, dit = get_res_dit(res_dict[sci_lst[i]])
             else:
                 resolution, dit = set_res(standard_res)
 
-            dit = [0.111, 1.3]
-
-            # Checks if list item is itself a list
+            # NOTE: Checks if list item is itself a list
             if isinstance(o, list):
                 for j, l in enumerate(o):
                     ob.mat_gen_ob(l, array_config, 'CAL', outdir=outdir,\
@@ -158,11 +189,43 @@ def make_cal_obs(cal_lst: List, sci_lst: List, tag_lst: List,
         print("Skipped OB - Check")
         print(e)
 
+def read_yaml_into_OBs(path2file: Path outpath: Path, mode: str) -> None:
+    """This reads the (.yaml)-file into a format suitable for the Jozsef
+    Varga's OB creation code and subsequently makes the OBs with it
+
+    Parameters
+    ----------
+    path2file: Path
+        The night plan (.yaml)-file
+    outpath: Path
+        The output path
+    mode: str
+        The mode of operation of MATISSE
+    """
+    run_dict = load_yaml(path2file)
+
+    for i, o in run_dict.items():
+        for j, l in o.items():
+            temp_path = os.path.join(outpath,\
+                                     i.split(",")[0].replace(' ', ''),\
+                                     j.split(":")[0].replace(' ', ''))
+
+            if not os.path.exists(temp_path):
+                os.makedirs(temp_path)
+
+            time.sleep(1)
+            night = SimpleNamespace(**l)
+            make_sci_obs(night.SCI, array_config, temp_path, res_dict, standard_res,
+                         obs_templates, acq_template, mode)
+            make_cal_obs(night.CAL, night.SCI, night.TAG, array_config,\
+                         temp_path, res_dict, standard_res,\
+                         obs_templates, acq_template, mode)
+
 def ob_pipeline(array_config: str, outpath: str, path2file: Optional[Path] = None,
                 manual_lst: Optional[List] = None, res_dict: Optional[Dict] = None,
-                standard_res: Optional[List] = ["LR", "LR"],
-                obs_templates: Optional[List] = [ob.obs_ft_tpl],
-                acq_template = ob.acq_ft_tpl) -> int:
+                standard_res: Optional[List] = None,
+                obs_templates: Optional[List] = None
+                acq_template = None, mode: str = "st") -> int:
     """Gets all functionality and automatically creates the OBs
 
     Parameters
@@ -178,32 +241,32 @@ def ob_pipeline(array_config: str, outpath: str, path2file: Optional[Path] = Non
     standard_res: List, optional
         The default spectral resolutions for L- and N-band. Set to low for both
         as a default
+    mode: str
+        The mode MATISSE is operated in and for which the OBs are created.
+        Either 'st' for standalone, 'gr' for GRA4MAT or 'both', if OBs for both
+        are to be created
+
+    Returns
+    -------
+    Execution_code
+        '1' for SUCCESS '-1' for ERROR
     """
-    if len(manual_lst[0]) != 0:
+    if manual_lst[0]:
         sci_lst, cal_lst, tag_lst = manual_lst
         make_sci_obs(sci_lst, array_config, outpath, res_dict, standard_res,\
-                     obs_templates, acq_template)
+                     obs_templates, acq_template, mode)
         make_cal_obs(cal_lst, sci_lst, tag_lst, array_config,\
                      outpath, res_dict, standard_res,\
-                     obs_templates, acq_template)
+                     obs_templates, acq_template, mode)
 
     elif path2file:
-        run_dict = load_yaml(path2file)
-        for i, o in run_dict.items():
-            for j, l in o.items():
-                temp_path = os.path.join(outpath,\
-                                         i.split(",")[0].replace(' ', ''),\
-                                         j.split(":")[0].replace(' ', ''))
-                if not os.path.exists(temp_path):
-                    os.makedirs(temp_path)
+        mode_lst = ["standalone", "GRA4MAT"] if mode == "both" else \
+                (["standalone"] if mode == "st" else ["GRA4MAT"])
+        print(mode_lst)
 
-                time.sleep(1)
-                night = SimpleNamespace(**l)
-                make_sci_obs(night.SCI, array_config, temp_path, res_dict, standard_res,
-                             obs_templates, acq_template)
-                make_cal_obs(night.CAL, night.SCI, night.TAG, array_config,\
-                             temp_path, res_dict, standard_res,\
-                             obs_templates, acq_template)
+        for i in mode_lst:
+            outpath = os.path.join(outpath, i)
+            read_yaml_into_OBs(path2file, outpath, i)
     else:
         raise IOError("Neither '.yaml'-file nor input list found!")
 
@@ -211,44 +274,16 @@ def ob_pipeline(array_config: str, outpath: str, path2file: Optional[Path] = Non
 
 
 if __name__ == "__main__":
-    # Get and parses the night plans Roy's script creates
-     try:
-         # path2file = os.path.join(os.getcwd(), "night_plan.yaml")
-        path2file = ""
-     except:
-         # If non valid path is given then default to empty string
-         print("No input file for parsing found or not readable!")
-         path2filea = None
+    path2file = ""
 
-     # Specifies the paths, where the '.obx'-files are saved to, name of run can
-     # be changed to actual one
-     outdir = "/Users/scheuck/Documents/PhD/matisse_stuff/observation/obmaking/obs/"
-     outpath = os.path.join(outdir)
+    outdir = "/Users/scheuck/Documents/PhD/matisse_stuff/observation/obmaking/obs/"
+    outpath = os.path.join(outdir)
 
-     # Manule use of the wrapper
-     # Example of usage, calibration lists accept sublists, more than one
-     # calibrator, but also single items, same for tag_lst
-     # sci_lst = ["AS 209", "VV Ser"]
-     # cal_lst = [["HD 142567", "HD 467893"], "]
-     # tag_lst = [["LN", "N"], "L"]
-     # sci_lst, cal_lst, tag_lst = [], [], []
-     sci_lst = []
-     cal_lst = []
-     tag_lst = []
+    sci_lst = []
+    cal_lst = []
+    tag_lst = []
 
-     # Specifies the res_dict, in the format. Can be left empty.
-     # Example of usage, at the moment only changes L-band resolution
-     # res_dict = {"AS 209": "MR"}
-     res_dict = {}
+    res_dict = {}
 
-     # Pipeline for ob creation, parseOB is commented out, templates can be
-     # changed
-     # Templates that can be used, for acq. only one at a time, for obs multiple
-     # in a list
-     # ob.acq_ft_tpl, ob.acq_tpl, ob.obs_tpl, ob.obs_ft_tpl, ob.obs_ft_coh_tpl,
-     # ob.obs_ft_vis_tpl
-     ob_pipeline("UTs", outpath, path2file,  manual_lst=[sci_lst, cal_lst, tag_lst],\
-                 res_dict=res_dict, standard_res=["LR", "LR"],\
-                 obs_templates=[ob.obs_tpl],
-                 acq_template=ob.acq_tpl)
-
+    ob_pipeline("UTs", outpath, path2file,  manual_lst=[sci_lst, cal_lst, tag_lst],\
+             res_dict=res_dict}
