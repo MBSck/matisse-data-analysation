0aa2cc637ee19d0a1cd7e301d18f66bd439e1bf4
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-05-18 16:26:42 +0200

    Update main.py
    Add better description of work process and password getter

--- observation/phase2/main.py
+++ observation/phase2/main.py
@@ -3,9 +3,25 @@
 __author__ = "Marten Scheuck"
 __date__   = "2022-05"
 
+import sys
+import getpass
+
 from parseOBplan import parse_night_plan
 from automatedOBcreation import ob_creation
 from uploader import ob_uploader
+
+def get_password():
+    """Gets the user's ESO-user password to access the P2"""
+    prompt = f'Input your ESO-user password: '
+    if sys.platform == 'ios':
+        import console
+        password = console.password_alert(prompt)
+    elif sys.stdin.isatty():
+        password = getpass.getpass(prompt)
+    else:
+        password = input()
+
+    return password
 
 def main():
     # Path to the night observing plan in (.txt)-format
@@ -21,9 +37,28 @@ def main():
     # The period and the proposal tag of the run
     run_data = ["109", "2313"]
 
-    run_dict = parse_night_plan(path, save2file=False)
-    ob_creation("medium", outpath, run_data=run_dict, res_dict=res_dict, mode="gr")
-    ob_uploader(outpath, "production", run_data, "MbS")
+    # Get user password for ESO
+    password = get_password()
+
+    print("Parsing the Night plan!")
+    print("-------------------------------------------------------------------")
+    run_dict = parse_night_plan(path, save2file=True)
+    print("Parsing complete!")
+    print("-------------------------------------------------------------------")
+    print("-------------------------------------------------------------------")
+
+    print("Creating the OBs!")
+    print("-------------------------------------------------------------------")
+    ob_creation("medium", outpath, run_data=run_dict,
+                res_dict=res_dict, mode="gr")
+    print("OB creation compete!")
+
+    print("-------------------------------------------------------------------")
+    print("-------------------------------------------------------------------")
+    print("Uploading the OBs!")
+    print("-------------------------------------------------------------------")
+    ob_uploader(p2, outpath, "production", run_data, "MbS", password)
+    print("Uploading complete!")
 
 
 if __name__ == "__main__":
