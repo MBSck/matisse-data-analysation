53d67f592fdddf0c07f8b0905de27f4b20944ebf
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-06-14 16:43:11 +0200

    Update model_fitting.py
    Add better docs

--- ppdmodler/src/functionality/model_fitting.py
+++ ppdmodler/src/functionality/model_fitting.py
@@ -1,7 +1,46 @@
 #!/usr/bin/env python3
 
 """Test file for a 2D-Gaussian PPD model, that is fit with MCMC; The emcee
-package"""
+package
+
+...
+
+Initial sets the theta
+
+>>> initial = np.array([1.5, 135, 1., 1., 100., 3., 0.01, 0.7])
+>>> priors = [[1., 2.], [0, 180], [0., 2.], [0., 2.], [0., 180.], [1., 10.],
+              [0., 1.], [0., 1.]]
+>>> labels = ["AXIS_RATIO", "P_A", "C_AMP", "S_AMP", "MOD_ANGLE", "R_INNER",
+              "TAU", "Q"]
+>>> bb_params = [1500, 7900, 19, 140]
+
+File to read data from
+
+>>> f = "../../assets/Final_CAL.fits"
+>>> out_path = "../../assets"
+
+sws is for L-band flux; timmi2 for the N-band flux
+
+>>> flux_file = "../../assets/HD_142666_timmi2.txt"
+
+Set the data, the wavelength has to be the fourth argument [3]
+
+>>> data = set_data(fits_file=f, flux_file=flux_file, pixel_size=100,
+                    sampling=128, wl_ind=38, zero_padding_order=3, vis2=False)
+
+Set the mcmc parameters and the data to be fitted.
+
+>>> mc_params = set_mc_params(initial=initial, nwalkers=50, niter_burn=100,
+                              niter=250)
+
+This calls the MCMC fitting
+
+>>> fitting = ModelFitting(CompoundModel, data, mc_params, priors, labels,
+                           numerical=True, vis=True, modulation=True,
+                           bb_params=bb_params, out_path=out_path)
+>>> fitting.pipeline()
+
+"""
 
 import os
 import yaml
@@ -20,27 +59,34 @@ from src.functionality.readout import ReadoutFits, read_single_dish_txt2np
 from src.functionality.genetic_algorithm import genetic_algorithm, decode
 from src.functionality.utilities import chi_sq
 
-# Interesting stuff -> Change p0, how it gets made and more
 
-# TODO: The code has some difficulties rescaling for higher pixel numbers
+# FIXME: The code has some difficulties rescaling for higher pixel numbers
 # and does in that case not approximate the right values for the corr_fluxes,
 # see pixel_scaling
 
+# TODO: Implement global parameter search algorithm (genetic algorithm)
+
+# TODO: Implement optimizer algorithm
+
 # TODO: Safe the model as a '.yaml'-file? Better than '.fits'?
 
-# TODO: Think of rewriting code so that params are taken differently
+# TODO: Think of rewriting code so that params are taken differently. Maybe
+# like in a computer game with an entity manager
+
 # TODO: Make documentation in Evernote of this file and then remove unncessary
 # comments
-# TODO: Make this more generally applicable
+
 # TODO: Make plots of the model + fitting, that show the visibility curve, and
-# two more that show the # fit of the visibilities and the closure phases to the measured one
+# two more that show the fit of the visibilities and the closure phases to the
+# measured one
+
 # TODO: Make one plot that shows a model of the start parameters and the
 # uv-points plotted on top, before fitting starts and options to then change
 # them in order to get the best fit (Even multiple times)
+
 # TODO: Make save paths for every class so that you can specify where files are
 # being saved to
 
-# Functions
 
 def set_data(fits_file: Path, pixel_size: int,
              sampling: int, flux_file: Path = None,
@@ -106,7 +152,7 @@ def set_data(fits_file: Path, pixel_size: int,
     uvcoords = readout.get_uvcoords()
     u, v = readout.get_split_uvcoords()
     t3phi_uvcoords = readout.get_t3phi_uvcoords()
-    data = (vis, viserr, cphase, cphaseerr, flux, fluxerr)
+    data = (vis, viserr, cphase, cphaseerr, flux, fluxerr, vis2)
 
     return (data, pixel_size, sampling, wavelength, uvcoords,
             u, v, zero_padding_order, t3phi_uvcoords)
@@ -133,29 +179,28 @@ def set_mc_params(initial: np.ndarray, nwalkers: int,
     tuple
         A tuple that contains (p0, nwalkers, ndim, niter_burn, niter)
     """
-    ndim = len(initial)
-    return (initial, nwalkers, ndim, niter_burn, niter)
+    return (initial, nwalkers, len(initial), niter_burn, niter)
 
 
 class ModelFitting:
     """"""
     def __init__(self, model, data: List, mc_params: List[float],
                  priors: List[List[float]], labels: List[str],
-                 numerical: bool = True, vis: bool = False,
-                 modulation: bool = False, bb_params: List = None,
-                 out_path: Path = None) -> None:
+                 numerical: bool = True, modulation: bool = False,
+                 bb_params: List = None, out_path: Path = None) -> None:
         self.priors, self.labels = priors, labels
         self.bb_params = bb_params
         self.model = model
 
-        self.numerical, self.vis, self.vis2 = numerical, vis, not vis
         self.modulation = modulation
 
         self.fr_scaling = 0
 
         self.data, self.pixel_size, self.sampling,self.wavelength,\
                 self.uvcoords, self.u, self.v,\
-                self.zero_padding_order, self.t3phi_uvcoords = data
+                self.zero_padding_order, self.t3phi_uvcoords, self.vis2 = data
+
+        self.numerical, self.vis = numerical, not self.vis2
 
         self.realdata, self.realdataerr,\
                 self.realcphase, self.realcphaserr,\
@@ -188,53 +233,46 @@ class ModelFitting:
         self.out_path = out_path
 
     def pipeline(self) -> None:
-        # n_bits = 8
-        # # Do global optimisation
-        # best, score = genetic_algorithm(self.lnlike, self.priors,
-        #                                 n_bits=n_bits, n_iter=1000, n_pop=100,
-        #                                 r_cross=0.85,
-        #                                 r_mut=(1.0/(n_bits*len(self.priors))),
-        #                                 k=3)
-        # print()
-        # print(best, score)
-        # decoded = decode(self.priors, n_bits, best)
-        # print(decoded)
+        """The main pipeline of this here fitting class
 
+        Gets the sampler, position and its probability from the
+        'do_fit()'-method and then does the corner plots and the posterior
+        spread and the resulting model.
+        """
         sampler, pos, prob, state = self.do_fit()
 
-        # This plots the corner plots of the posterior spread
         self.plot_corner(sampler)
-
-        # This plots the resulting model
         self.plot_model_and_vis_curve(sampler)
 
-        # This saves the best-fit model data and the real data
-        # self.dump2yaml()
-
-    def optimise_inital_theta(self):
-        """Run a scipy optimisation on the initial values to get theta"""
-        return scipy.optimize.minimize(self.lnlike, x0=self.initial,
-                                       bounds=self.priors)
-
     def do_fit(self) -> np.array:
-        """The main pipline that executes the combined mcmc code and fits the
-        model"""
-        # The EnsambleSampler gets the parameters. The args are the args put into the
-        # lob_prob function. Additional parameter a can be used for the stepsize. None is
-        # the default
+        """Runs the emcee fit
+
+        The EnsambleSampler recieves the parameters and the args are passed to
+        the 'log_prob()' method (an addtional parameter 'a' can be used to
+        determine the stepsize, defaults to None).
+
+        The burn-in is first run to explore the parameter space and then the
+        walkers settle into the maximum of the density. The state variable is
+        then passed to the production run.
+
+        The chain is reset before the production with the state variable being
+        passed. 'rstate0' is the state of the internal random number generator
+
+        Returns
+        -------
+        sampler
+        pos
+        prob
+        state
+        """
         sampler = emcee.EnsembleSampler(self.nw, self.nd, self.lnprob)
 
-        # Burn-in of the sampler. Explores the parameter space. The walkers get settled
-        # into the maximum of the density. Saves the walkers in the state variable
         print("Running burn-in...")
         p0 = sampler.run_mcmc(self.p0, self.nib, progress=True)
         print("--------------------------------------------------------------")
 
-        # Resets the chain to remove burn in samples and sets the walkers lower
         sampler.reset()
 
-        # Do production. Starts from the final position of burn-in chain (rstate0 is
-        # the state of the internal random number generator)
         print("Running production...")
         pos, prob, state = sampler.run_mcmc(p0, self.ni, progress=True)
         print("--------------------------------------------------------------")
@@ -264,7 +302,8 @@ class ModelFitting:
     def lnlike(self, theta: np.ndarray):
         """Takes theta vector and the x, y and the yerr of the theta.
         Returns a number corresponding to how good of a fit the model is to your
-        data for a given set of parameters, weighted by the data points.  That it is more important"""
+        data for a given set of parameters, weighted by the data points.
+        """
         datamod, cphasemod, tot_flux = self.model4fit_numerical(theta)
         datamod = np.insert(datamod, 0, tot_flux)
 
@@ -337,7 +376,6 @@ class ModelFitting:
         model_img = model.eval_model(self.theta_max, self.pixel_size, 4097)
         self.datamod = np.insert(self.datamod, 0, tot_flux)
 
-        # Correspond the best fit to the uv coords
         print("Best fit corr. fluxes:")
         print(datamod)
         print("Real corr. fluxes")
@@ -391,7 +429,7 @@ class ModelFitting:
 
     def plot_corner(self, sampler) -> None:
         """Plots the corner plot of the posterior spread"""
-        samples = sampler.get_chain(flat=True)  # Or sampler.flatchain
+        samples = sampler.get_chain(flat=True)
         fig = corner.corner(samples, show_titles=True, labels=self.labels,
                            plot_datapoints=True, quantiles=[0.16, 0.5, 0.84])
         save_path = f"{self.model_init.name}_corner_plot_{self.wavelength*1e6:.2f}.png"
@@ -400,44 +438,8 @@ class ModelFitting:
         else:
             plt.savefig(os.path.join(self.out_path, save_path))
 
-    def test_model(self, sampler) -> None:
-        # TODO: Implement printout of theta_max
-        # Another test of whether or not the sampling went well is the mean acceptance
-        # fraction and the integrated autocorrelation time
-        # Acceptance fraction has an entry for each walker -> It is a vector with the
-        # dimensions of the steps
-        # As a rule of thumb if the acceptance_fraction is below 0.2, the a parameter
-        # needs to be decreased, and if it is above 0.5 then the a parameter needs to
-        # be increased
-        acceptance = np.mean(sampler.acceptance_fraction)
-
-        # The autocorrelation time is a vector with ndim dimensions
-        autocorr= np.mean(sampler.get_autocorr_time())
-        print(f"Mean acceptance fraction {acceptance} and the autcorrelation time {autocorr}")
-
-    def dump2yaml(self) -> None:
-        """Dumps the resulting data of the model into a '.yaml'-file"""
-        # TODO: Make this work
-        if self.vis:
-            vis_dict = {"visamp": self.realdata[0].tolist(),
-                        "visphase": self.realdata[1].tolist(),
-                        "total_flux@wl": self.realflux}
-        else:
-            vis_dict = {"vis2data": self.realdata.tolist()}
-
-        fit_dict = {"datamod": self.datamod.tolist(),
-                    "total_flux": self.total_flux_fit,
-                    "Best fit - theta": self.theta_max,
-                    "labels": self.labels,}
-
-        model_dict = {self.model_init.name: {"Real data": vis_dict, "Fit values": fit_dict}}
-        save_path = os.path.join(self.out_path, f"{self.model_init.name}_model_after_fit.yaml")
-        with open(save_path, "w") as fy:
-            yaml.safe_dump(model_dict, fy)
-
 
 if __name__ == "__main__":
-    # Initial sets the theta
     initial = np.array([1.5, 135, 1., 1., 100., 3., 0.01, 0.7])
     priors = [[1., 2.], [0, 180], [0., 2.], [0., 2.], [0., 180.], [1., 10.],
               [0., 1.], [0., 1.]]
@@ -445,22 +447,14 @@ if __name__ == "__main__":
               "TAU", "Q"]
     bb_params = [1500, 7900, 19, 140]
 
-    # File to read data from
     f = "../../assets/Final_CAL.fits"
     out_path = "../../assets"
-
-    # sws is for L-band flux; timmi2 for the N-band flux
     flux_file = "../../assets/HD_142666_timmi2.txt"
 
-    # Set the data, the wavelength has to be the fourth argument [3]
     data = set_data(fits_file=f, flux_file=flux_file, pixel_size=100,
                     sampling=128, wl_ind=38, zero_padding_order=3, vis2=False)
-
-    # Set the mcmc parameters and the data to be fitted.
     mc_params = set_mc_params(initial=initial, nwalkers=50, niter_burn=100,
                               niter=250)
-
-    # This calls the MCMC fitting
     fitting = ModelFitting(CompoundModel, data, mc_params, priors, labels,
                            numerical=True, vis=True, modulation=True,
                            bb_params=bb_params, out_path=out_path)
